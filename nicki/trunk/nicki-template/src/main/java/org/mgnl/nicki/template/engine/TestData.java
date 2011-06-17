d0ed402a9e55b5cfd9a39223345e57e793bbcfe5
rhi <rhi@92c39532-7ad5-11e0-8644-75d2b71920e0>
2011-06-17 09:08:23 +0000

    Initial import.

---
+++ nicki/trunk/nicki-template/src/main/java/org/mgnl/nicki/template/engine/TestData.java
@@ -0,0 +1,102 @@
+package org.mgnl.nicki.template.engine;
+
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.mgnl.nicki.dynamic.objects.objects.Function;
+import org.mgnl.nicki.ldap.context.NickiContext;
+import org.mgnl.nicki.ldap.context.Target;
+import org.mgnl.nicki.ldap.context.TargetFactory;
+import org.mgnl.nicki.ldap.objects.DynamicObject;
+
+public class TestData {
+
+	public static final String SEPARATOR = "=";
+	private DynamicObject dynamicObject;
+	
+	public TestData(DynamicObject dynamicObject) {
+		super();
+		this.dynamicObject = dynamicObject;
+	}
+
+	public Target getTarget() {
+		@SuppressWarnings("unchecked")
+		List<String> testData = (List<String>) dynamicObject.get("testData");
+		if (testData != null) {
+			for (Iterator<String> iterator = testData.iterator(); iterator.hasNext();) {
+				String value = iterator.next();
+				String name = StringUtils.substringBefore(value, SEPARATOR);
+				String data = StringUtils.substringAfter(value, SEPARATOR);
+				if (StringUtils.equals(name, "target")) {
+					if (StringUtils.isNotEmpty(data)) {
+						Target t = TargetFactory.getTarget(data);
+						if (t != null) {
+							return t;
+						}
+					}
+				}
+			}
+		}
+		return TargetFactory.getDefaultTarget();
+	}
+
+	public Map<String, Object> getData(NickiContext context) {
+		
+
+		// Create the root hash
+		Map<String, Object> root = new HashMap<String, Object>();
+		Function function = null;
+		try {
+			function = new Function(context);
+			function.setContext(context);
+			function.initDataModel();
+			root.put("function", function);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return root;
+	}
+	
+	public  Map<String, String> getObjects() {
+		Map<String, String> objects = new HashMap<String, String>();
+		objects.put("person", "cn=ablake,ou=users,o=utopia");
+		return objects;
+	}
+	
+	public Map<String, Object> getDataModel(NickiContext context) {
+		Map<String, Object> dataModel = new HashMap<String, Object>();
+		Function function = null;
+		try {
+			function = new Function(context);
+			function.setContext(context);
+			function.initDataModel();
+			dataModel.put("function", function);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		@SuppressWarnings("unchecked")
+		List<String> testData = (List<String>) dynamicObject.get("testData");
+		if (testData != null) {
+			for (Iterator<String> iterator = testData.iterator(); iterator.hasNext();) {
+				String value = iterator.next();
+				String name = StringUtils.substringBefore(value, SEPARATOR);
+				String data = StringUtils.substringAfter(value, SEPARATOR);
+				if (StringUtils.isNotEmpty(data)) {
+					if (context.isExist(data)) {
+						dataModel.put(name, context.loadObject(data));
+					} else {
+						dataModel.put(name, data);
+					}
+				}
+			}
+		}
+		return dataModel;
+	}
+
+
+}
+	
