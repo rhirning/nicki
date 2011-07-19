5d3d5b9ef5041506f0226be7c9367e4911465b3d
cna <cna@92c39532-7ad5-11e0-8644-75d2b71920e0>
2011-07-19 10:50:06 +0000

    Methode getName verschoben von CartActionHandler nach BasicActionHandler

--- nicki-dynamic-objects/src/main/java/org/mgnl/nicki/dynamic/objects/wrapper/CartActionHandler.java
+++ nicki-dynamic-objects/src/main/java/org/mgnl/nicki/dynamic/objects/wrapper/CartActionHandler.java
@@ -2,11 +2,8 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.mgnl.nicki.dynamic.objects.wrapper;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Map;
 import org.mgnl.nicki.ldap.context.NickiContext;
 
@@ -19,19 +16,19 @@ public class CartActionHandler extends BasicActionHandler {
 	private static final String ACTIONNAME = "cart-action";
 
 	private static enum ACTION_TYPE {
+
 		ADD("add"),
 		MODIFY("modify"),
 		DELETE("delete");
-		
 		private String value;
 
 		private ACTION_TYPE(String value) {
 			this.value = value;
 		}
-		
+
 		public String getValue() {
 			return value;
-		}	
+		}
 	}
 
 	public CartActionHandler(NickiContext ctx, String target) {
@@ -45,11 +42,11 @@ public class CartActionHandler extends BasicActionHandler {
 	public void addAttributes(String item, Map<String, String> attributes) {
 		putAttributes(ACTION_TYPE.ADD, item, attributes);
 	}
-	
+
 	public void modify(String item) {
 		putItem(ACTION_TYPE.MODIFY, item);
 	}
-	
+
 	public void modifyAttributes(String item, Map<String, String> attributes) {
 		putAttributes(ACTION_TYPE.MODIFY, item, attributes);
 	}
@@ -60,12 +57,6 @@ public class CartActionHandler extends BasicActionHandler {
 
 	public void deleteAttributes(String item, Map<String, String> attributes) {
 		putAttributes(ACTION_TYPE.DELETE, item, attributes);
-	}
-
-	@Override
-	protected String getName() {
-		SimpleDateFormat sdf = new SimpleDateFormat("-yyyyMMdd-HH-mm-ss-SSS");
-		return ACTIONNAME + sdf.format(new Date());
 	}
 
 	private void putItem(ACTION_TYPE action, String item) {
