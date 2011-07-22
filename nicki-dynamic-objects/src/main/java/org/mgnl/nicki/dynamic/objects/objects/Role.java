1680d997e3680cc74ba880dd757ab9a2c3ec0a53
cna <cna@92c39532-7ad5-11e0-8644-75d2b71920e0>
2011-07-22 11:37:07 +0000

    Approvers in Rolle aufgenommen

--- nicki-dynamic-objects/src/main/java/org/mgnl/nicki/dynamic/objects/objects/Role.java
+++ nicki-dynamic-objects/src/main/java/org/mgnl/nicki/dynamic/objects/objects/Role.java
@@ -15,11 +15,19 @@ public class Role extends DynamicStructObject {
 		DynamicAttribute dynAttribute = new DynamicAttribute("name", "cn", String.class);
 		dynAttribute.setNaming();
 		addAttribute(dynAttribute);
-		dynAttribute = new DynamicReference("resourceAssociations", "o=system", "nrfRole", String.class);
+
+                dynAttribute = new DynamicReference("resourceAssociations", "o=system", "nrfRole", String.class);
 		addAttribute(dynAttribute);
+
+                // TODO - o=utopia kann wohl nicht stimmen
 		dynAttribute = new DynamicReference("member", "o=utopia", "nrfAssignedRoles", String.class);
 		dynAttribute.setMultiple();
 		dynAttribute.setForeignKey();
+		addAttribute(dynAttribute);
+
+		dynAttribute = new DynamicAttribute("approver", "nrfApprovers", String.class);
+		//dynAttribute.setForeignKey();
+		dynAttribute.setMultiple();
 		addAttribute(dynAttribute);
 	};
 	
