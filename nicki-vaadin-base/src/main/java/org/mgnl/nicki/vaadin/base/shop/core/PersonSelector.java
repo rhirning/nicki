202a901150c5fb141856065b9375ac7a2fd04286
rhi <31392706+rhirning@users.noreply.github.com>
2011-08-16 07:44:07 +0000

    Aktiv/inaktiv/alle - Flag bei Personensuche eingebaut

--- nicki-vaadin-base/src/main/java/org/mgnl/nicki/vaadin/base/shop/core/PersonSelector.java
+++ nicki-vaadin-base/src/main/java/org/mgnl/nicki/vaadin/base/shop/core/PersonSelector.java
@@ -7,6 +7,6 @@ import com.vaadin.ui.ComponentContainer;
 
 public interface PersonSelector extends ComponentContainer {
 
-	void init(NickiContext context, SelectPersonCommand selectPersonCommand);
+	void init(NickiContext context, boolean useActiveInactive, SelectPersonCommand selectPersonCommand);
 
 }
