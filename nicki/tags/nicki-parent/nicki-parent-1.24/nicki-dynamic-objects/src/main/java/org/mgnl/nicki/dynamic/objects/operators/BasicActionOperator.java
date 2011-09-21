b4e573eb53e7f21bbe8407078f50e4aa3019a640
rhi <rhi@92c39532-7ad5-11e0-8644-75d2b71920e0>
2011-09-21 10:03:36 +0000

    [maven-release-plugin]  copy for tag nicki-parent-1.24

---
+++ nicki/tags/nicki-parent/nicki-parent-1.24/nicki-dynamic-objects/src/main/java/org/mgnl/nicki/dynamic/objects/operators/BasicActionOperator.java
@@ -0,0 +1,158 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mgnl.nicki.dynamic.objects.operators;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.mgnl.nicki.core.config.Config;
+import org.mgnl.nicki.dynamic.objects.objects.BasicAction;
+import org.mgnl.nicki.ldap.context.NickiContext;
+import org.mgnl.nicki.ldap.data.InstantiateDynamicObjectException;
+import org.mgnl.nicki.ldap.objects.DynamicObjectException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSSerializer;
+
+/**
+ *
+ * @author cna
+ */
+public class BasicActionOperator {
+
+	private NickiContext ctx;
+	private Document doc;
+	private String target;
+	private String actionname;
+	private boolean noexec;
+        protected static final SimpleDateFormat STANDARD_DATE_FORMAT = new SimpleDateFormat("yyyyMMdd");
+
+	public enum DN_FORMAT {
+
+		LDAP("ldap"),
+		SLASH("slash");
+		private String value;
+
+		private DN_FORMAT(String value) {
+			this.value = value;
+		}
+
+		public String getValue() {
+			return value;
+		}
+	}
+
+	@SuppressWarnings("OverridableMethodCallInConstructor")
+	public BasicActionOperator(NickiContext ctx, String actionname, String target, boolean noexec) {
+		this.ctx = ctx;
+		this.actionname = actionname;
+		this.target = target;
+		this.noexec = noexec;
+
+		initDocument();
+	}
+
+	public void setStringParam(String name, String value) {
+		Element elem = doc.createElement("param");
+
+
+		elem.setAttribute("name", name);
+		elem.appendChild(doc.createTextNode(value));
+
+		doc.getFirstChild().appendChild(elem);
+	}
+
+	public void setDnParam(String name, DN_FORMAT format, String value) {
+		Element elem = doc.createElement("param");
+
+		elem.setAttribute("name", name);
+		elem.setAttribute("type", "dn");
+		elem.setAttribute("format", format.getValue());
+
+		elem.appendChild(doc.createTextNode(value));
+
+		doc.getFirstChild().appendChild(elem);
+	}
+
+	public void setListParam(String name, List<String> values) {
+		Element list = doc.createElement("paramlist");
+		Node node;
+
+		list.setAttribute("name", name);
+
+		for (String string : values) {
+			node = doc.createElement("param");
+			node.appendChild(doc.createTextNode(string));
+
+			list.appendChild(node);
+		}
+
+		doc.getFirstChild().appendChild(list);
+	}
+
+	public void setDnListParam(String name, DN_FORMAT format, List<String> values) {
+		Element list = doc.createElement("paramlist");
+		Element node;
+
+		list.setAttribute("name", name);
+		list.setAttribute("type", "dn");
+		list.setAttribute("format", format.getValue());
+
+		for (String string : values) {
+			node = doc.createElement("param");
+			node.appendChild(doc.createTextNode(string));
+
+			list.appendChild(node);
+		}
+
+		doc.getFirstChild().appendChild(list);
+	}
+
+	public BasicAction create() throws InstantiateDynamicObjectException, DynamicObjectException {
+		BasicAction action = ctx.getObjectFactory().getNewDynamicObject(BasicAction.class, Config.getProperty("nicki.actions.basedn"), getName());
+		action.setParameter(getXml());
+		action.setNoExec(noexec);
+		action.setTarget(target);
+		action.setActionName(actionname);
+
+		System.out.println("Going to create " + action.getActionName() + " in " + Config.getProperty("nicki.actions.basedn"));
+		action.create();
+
+		return action;
+	}
+
+	protected String getName() {
+		SimpleDateFormat sdf = new SimpleDateFormat("-yyyyMMdd-HH-mm-ss-SSS");
+		return actionname + sdf.format(new Date());
+	}
+
+	@SuppressWarnings("CallToThreadDumpStack")
+	protected void initDocument() {
+		DocumentBuilderFactory domfactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = null;
+		try {
+			builder = domfactory.newDocumentBuilder();
+		} catch (ParserConfigurationException ex) {
+//			Should never happen
+			ex.printStackTrace();
+			assert 0 == 1;
+		}
+		doc = builder.newDocument();
+
+		Element root = doc.createElement("parameterset");
+		doc.appendChild(root);
+	}
+
+	private String getXml() {
+		DOMImplementationLS impl = (DOMImplementationLS) doc.getImplementation();
+		LSSerializer serializer = impl.createLSSerializer();
+		return serializer.writeToString(doc);
+	}
+}
---
+++ nicki/tags/nicki-parent/nicki-parent-1.24/nicki-shop/src/main/java/org/mgnl/nicki/shop/catalog/CatalogArticleAttribute.java
@@ -0,0 +1,68 @@
+package org.mgnl.nicki.shop.catalog;
+
+import java.io.Serializable;
+
+import org.jdom.Element;
+import org.mgnl.nicki.core.i18n.I18n;
+
+@SuppressWarnings("serial")
+public class CatalogArticleAttribute implements Serializable{
+	public static final String SEPARATOR = "|"; 
+	private String name;
+	private String key;
+	private String type;
+	private String contentClass;
+	public CatalogArticleAttribute(String name, String key, String type) {
+		this.name = name;
+		this.key = key;
+		this.type = type;
+	}
+
+	public CatalogArticleAttribute(Element attributeElement) {
+		this.name = attributeElement.getAttributeValue("name");
+		this.key = attributeElement.getAttributeValue("label");
+		this.type = attributeElement.getAttributeValue("type");
+		this.contentClass = attributeElement.getAttributeValue("contentClass");
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLabel() {
+		return I18n.getText(key);
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public String getAttributeString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append(name).append(SEPARATOR).append(key).append(SEPARATOR).append(type);
+		return sb.toString();
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	public String getKey() {
+		return key;
+	}
+	
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("[catalogArticleAttribute name='").append(getName());
+		sb.append("' label='").append(getLabel());
+		sb.append("' key='").append(getKey());
+		sb.append("' type='").append(getType());
+		sb.append("']\n");
+		return sb.toString();
+	}
+
+	public String getContentClass() {
+		return contentClass;
+	}
+}
