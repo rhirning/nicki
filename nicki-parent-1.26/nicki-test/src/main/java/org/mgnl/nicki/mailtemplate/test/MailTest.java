098cf6b2d1fa64e4cc96d2e54edba9185548e23f
rhi <31392706+rhirning@users.noreply.github.com>
2011-10-20 11:04:03 +0000

    [maven-release-plugin]  copy for tag nicki-parent-1.26

---
+++ nicki-parent-1.26/nicki-test/src/main/java/org/mgnl/nicki/mailtemplate/test/MailTest.java
@@ -0,0 +1,28 @@
+package org.mgnl.nicki.mailtemplate.test;
+
+import java.io.IOException;
+
+import org.mgnl.nicki.ldap.auth.InvalidPrincipalException;
+import org.mgnl.nicki.ldap.context.AppContext;
+import org.mgnl.nicki.mailtemplate.engine.MailTemplateEngine;
+import org.mgnl.nicki.mailtemplate.model.MailWrapper;
+
+import freemarker.template.TemplateException;
+
+public class MailTest {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 * @throws TemplateException 
+	 * @throws InvalidPrincipalException 
+	 */
+	public static void main(String[] args) throws IOException, TemplateException, InvalidPrincipalException {
+		MailWrapper wrapper = 
+		MailTemplateEngine.executeMailTemplate("/mail1", TestData.getDataModel(AppContext.getSystemContext()));
+		System.out.println(wrapper.getMailPart(MailTemplateEngine.LOCALE_DE, MailTemplateEngine.MAILPART_SUBJECT));
+		System.out.println(wrapper.getMailPart(MailTemplateEngine.LOCALE_DE, MailTemplateEngine.MAILPART_HEADER));
+		System.out.println(wrapper.getMailPart(MailTemplateEngine.LOCALE_DE, MailTemplateEngine.MAILPART_FOOTER));
+	}
+
+}
