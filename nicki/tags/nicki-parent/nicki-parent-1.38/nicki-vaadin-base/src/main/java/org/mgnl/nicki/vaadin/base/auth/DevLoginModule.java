6d0c541be288f5cd2310f72844df46a912468864
rhi <rhi@92c39532-7ad5-11e0-8644-75d2b71920e0>
2011-11-23 13:26:44 +0000

    [maven-release-plugin]  copy for tag nicki-parent-1.38

---
+++ nicki/tags/nicki-parent/nicki-parent-1.38/nicki-vaadin-base/src/main/java/org/mgnl/nicki/vaadin/base/auth/DevLoginModule.java
@@ -0,0 +1,42 @@
+package org.mgnl.nicki.vaadin.base.auth;
+
+import javax.security.auth.login.LoginException;
+import javax.security.auth.spi.LoginModule;
+
+import org.apache.commons.lang.StringUtils;
+import org.mgnl.nicki.ldap.auth.NickiLoginModule;
+import org.mgnl.nicki.ldap.auth.NickiPrincipal;
+import org.mgnl.nicki.ldap.context.AppContext;
+
+public class DevLoginModule extends NickiLoginModule implements LoginModule {
+
+	@Override
+	public boolean login() throws LoginException {
+		// prompt for a user name and password
+		if (getCallbackHandler() == null)
+			throw new LoginException("Error: no CallbackHandler available "
+					+ "to garner authentication information from the user");
+		NickiPrincipal principal = null;
+		try {
+			 principal = AppContext.getSystemContext().getPrincipal();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		String username = principal.getName();
+		String password = principal.getPassword();
+
+		if (StringUtils.isNotEmpty(username) && StringUtils.isNotEmpty(password)) {
+			setSucceeded(true);
+			setUsername(username);
+			setPassword(password);
+			return true;
+		} else {
+			setSucceeded(false);
+			username = null;
+			password = null;
+			setUsername(username);
+			setPassword(password);
+			return false;
+		}
+	}
+}
