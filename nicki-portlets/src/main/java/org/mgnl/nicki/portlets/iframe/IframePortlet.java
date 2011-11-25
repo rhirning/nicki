c43d702dccb97b66a2a0e330ec9fb51e187c5019
rhi <31392706+rhirning@users.noreply.github.com>
2011-11-25 09:12:23 +0000

    IframeAdapter

--- nicki-portlets/src/main/java/org/mgnl/nicki/portlets/iframe/IframePortlet.java
+++ nicki-portlets/src/main/java/org/mgnl/nicki/portlets/iframe/IframePortlet.java
@@ -4,9 +4,13 @@ import java.io.IOException;
 
 import javax.portlet.GenericPortlet;
 import javax.portlet.PortletException;
+import javax.portlet.PortletPreferences;
 import javax.portlet.PortletRequestDispatcher;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
+
+import org.apache.commons.codec.binary.Base64;
+import org.mgnl.nicki.novell.userapp.jaas.UserAppAdapter;
 
 public class IframePortlet extends GenericPortlet {
 
@@ -21,6 +25,7 @@ public class IframePortlet extends GenericPortlet {
 			String pageName) throws PortletException {
 		response.setContentType(request.getResponseContentType());
 		request.setAttribute("javax.portlet.request", request);
+		request.setAttribute("nickiUrl", getUrl(request));
 		if (pageName == null || pageName.length() == 0) {
 			throw new NullPointerException("null or empty page name");
 		}
@@ -31,5 +36,16 @@ public class IframePortlet extends GenericPortlet {
 		} catch (IOException ioe) {
 			throw new PortletException(ioe);
 		}
+	}
+	
+	private String getUrl(RenderRequest request) {
+		  PortletPreferences pref = request.getPreferences();
+		  String url = pref.getValue("url", "");
+		  UserAppAdapter uaa = new UserAppAdapter();
+		  String user = uaa.getName(request);
+		  char passwd[] = uaa.getPassword(request);
+		  String args = "?nickiName=" + Base64.encodeBase64(user.getBytes()) + "nickiPassword="
+				  + Base64.encodeBase64(new String(passwd).getBytes());
+		  return url + args;
 	}
 }
