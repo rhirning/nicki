9216772b83d492db9e5d29507c98f0f400bbf807
cna <cna@92c39532-7ad5-11e0-8644-75d2b71920e0>
2012-02-02 22:07:18 +0000

    Incident #1: DataModel für die Templates auf XML umgestellt + XmlHelper-Klasse

--- nicki-template/src/main/java/org/mgnl/nicki/template/engine/TemplateHelper.java
+++ nicki-template/src/main/java/org/mgnl/nicki/template/engine/TemplateHelper.java
@@ -1,42 +1,39 @@
 /**
- * This file Copyright (c) 2003-2011 Dr. Ralf Hirning
- * All rights reserved.
+ * This file Copyright (c) 2003-2011 Dr. Ralf Hirning All rights reserved.
  *
  *
- * This file is dual-licensed under both the GNU General
- * Public License and an individual license with Dr. Ralf
- * Hirning.
+ * This file is dual-licensed under both the GNU General Public License and an individual license
+ * with Dr. Ralf Hirning.
  *
- * This file is distributed in the hope that it will be
- * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
- * Redistribution, except as permitted by whichever of the GPL
- * or the individual license, is prohibited.
+ * This file is distributed in the hope that it will be useful, but AS-IS and WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, TITLE,
+ * or NONINFRINGEMENT. Redistribution, except as permitted by whichever of the GPL or the individual
+ * license, is prohibited.
  *
- * 1. For the GPL license (GPL), you can redistribute and/or
- * modify this file under the terms of the GNU General
- * Public License, Version 3, as published by the Free Software
- * Foundation.  You should have received a copy of the GNU
- * General Public License, Version 3 along with this program;
- * if not, write to the Free Software Foundation, Inc., 51
- * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * 1. For the GPL license (GPL), you can redistribute and/or modify this file under the terms of the
+ * GNU General Public License, Version 3, as published by the Free Software Foundation. You should
+ * have received a copy of the GNU General Public License, Version 3 along with this program; if
+ * not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
  *
- * 2. For the individual license, this file and the accompanying
- * materials are made available under the terms of the
- * individual license.
+ * 2. For the individual license, this file and the accompanying materials are made available under
+ * the terms of the individual license.
  *
- * Any modifications to this file must keep this entire header
- * intact.
+ * Any modifications to this file must keep this entire header intact.
  *
  */
 package org.mgnl.nicki.template.engine;
 
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import org.apache.commons.lang.StringUtils;
 
 import org.mgnl.nicki.dynamic.objects.objects.Template;
+import org.mgnl.nicki.ldap.auth.InvalidPrincipalException;
+import org.mgnl.nicki.ldap.context.AppContext;
 import org.mgnl.nicki.ldap.context.NickiContext;
 import org.mgnl.nicki.ldap.objects.DynamicObject;
 import org.mgnl.nicki.template.handler.TemplateHandler;
@@ -55,7 +52,76 @@ public class TemplateHelper {
 		}
 		return dataModel;
 	}
-	
+
+	/**
+	 *
+	 * @param xmlDataModel 
+	 *	<datamodel> 
+	 *		<entry name="" value="" /> 
+	 *		<function name="" class=""> 
+	 *			<param name="" value="" />
+	 *		</function>
+	 *		<object name="" dn="" target="" <= optional />
+	 *	</datamodel>
+	 * @return dataModel
+	 */
+	public static Map<String, Object> getDataModel(String xmlDataModel) throws InvalidPrincipalException {
+		Map<String, Object> dataModel = new HashMap<String, Object>();
+
+		DataModelDescription model = DataModelDescription.fromXml(xmlDataModel);
+
+		for (DMEntry entry : model.getEntries()) {
+			dataModel.put(entry.name, entry.value);
+		}
+
+		for (DMFunction function : model.getFunctions()) {
+			try {
+				Object f = Class.forName((String) function.clazz).newInstance();
+
+				Method m;
+				for (DMParam param : (List<DMParam>) function.param) {
+					m = f.getClass().getDeclaredMethod("set" + StringUtils.capitalize(param.name), String.class);
+					m.invoke(param.value);
+				}
+
+				dataModel.put((String) function.name, f);
+			} catch (Exception e) {
+				System.err.println("could not create class " + (String) function.clazz + ": " + e.getMessage());
+			}
+		}
+		
+		NickiContext defaultContext = null;
+		try {
+			defaultContext = AppContext.getSystemContext();
+		} catch (InvalidPrincipalException e) {
+			System.err.println("could not create default system context: " + e.getMessage());
+			return null;
+		}
+		
+		NickiContext actualContext = null;
+		DynamicObject obj = null;
+
+		for (DMObject object : model.getObjects()) {
+			try {
+				if (StringUtils.isNotBlank(object.target)) {
+					actualContext = AppContext.getSystemContext(object.target);
+				} else {
+					actualContext = defaultContext;
+				}
+				obj = actualContext.loadObject(object.dn);
+				if (obj != null) {
+					dataModel.put(object.name, obj);
+				}
+			} catch (Exception e) {
+				System.err.println("could not load " + object.dn + ": " + e.getMessage());
+			}
+
+		}
+
+
+		return dataModel;
+	}
+
 	public static TemplateHandler getTemplateHandler(Template template) {
 		TemplateHandler handler = null;
 		if (template.hasHandler()) {
@@ -71,5 +137,4 @@ public class TemplateHelper {
 		}
 		return handler;
 	}
-
 }
