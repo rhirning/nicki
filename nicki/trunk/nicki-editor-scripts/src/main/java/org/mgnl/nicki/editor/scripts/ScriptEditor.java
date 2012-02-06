d8ce3eccc577936b82ff1be61df8a55f295e151e
rhi <rhi@92c39532-7ad5-11e0-8644-75d2b71920e0>
2012-02-06 10:21:58 +0000

    Import / Export

--- nicki/trunk/nicki-editor-scripts/src/main/java/org/mgnl/nicki/editor/scripts/ScriptEditor.java
+++ nicki/trunk/nicki-editor-scripts/src/main/java/org/mgnl/nicki/editor/scripts/ScriptEditor.java
@@ -35,13 +35,17 @@ package org.mgnl.nicki.editor.scripts;
 
 import org.mgnl.nicki.core.config.Config;
 import org.mgnl.nicki.core.helper.DataHelper;
+import org.mgnl.nicki.core.i18n.I18n;
 import org.mgnl.nicki.dynamic.objects.objects.Org;
 import org.mgnl.nicki.dynamic.objects.objects.Script;
+import org.mgnl.nicki.dynamic.objects.objects.Template;
 import org.mgnl.nicki.ldap.context.Target;
 import org.mgnl.nicki.ldap.context.TargetFactory;
 import org.mgnl.nicki.vaadin.base.application.NickiApplication;
 import org.mgnl.nicki.vaadin.base.editor.DynamicObjectRoot;
+import org.mgnl.nicki.vaadin.base.editor.ExportTreeAction;
 import org.mgnl.nicki.vaadin.base.editor.Icon;
+import org.mgnl.nicki.vaadin.base.editor.ImportTreeAction;
 import org.mgnl.nicki.vaadin.base.editor.ShowAllFilter;
 import org.mgnl.nicki.vaadin.base.editor.DataProvider;
 import org.mgnl.nicki.vaadin.base.editor.TreeEditor;
@@ -68,6 +72,9 @@ public class ScriptEditor extends NickiApplication {
 		editor.configureClass(Org.class, Icon.FOLDER, TreeEditor.CREATE.ALLOW, TreeEditor.DELETE.ALLOW, TreeEditor.RENAME.ALLOW, Org.class, Script.class );
 		editor.configureClass(Script.class, Icon.DOCUMENT, TreeEditor.CREATE.ALLOW, TreeEditor.DELETE.ALLOW, TreeEditor.RENAME.ALLOW);
 		editor.setClassEditor(Script.class, scriptViewer);
+		editor.addAction(new ImportTreeAction(editor, Org.class, I18n.getText(getI18nBase() + ".action.import"), getI18nBase()));
+		editor.addAction(new ExportTreeAction(getNickiContext(), Org.class, I18n.getText(getI18nBase() + ".action.export"), getI18nBase()));
+		editor.addAction(new ExportTreeAction(getNickiContext(), Script.class, I18n.getText(getI18nBase() + ".action.export"), getI18nBase()));
 		editor.initActions();
 		
 		return editor;
