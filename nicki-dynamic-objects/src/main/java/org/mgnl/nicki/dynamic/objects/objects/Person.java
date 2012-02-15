ea869c3b0fb9db6212c3ad6067c6c936bc646693
rhi <31392706+rhirning@users.noreply.github.com>
2012-02-15 12:56:15 +0000

    Anpassungen für Shop-Funktionalität

--- nicki-dynamic-objects/src/main/java/org/mgnl/nicki/dynamic/objects/objects/Person.java
+++ nicki-dynamic-objects/src/main/java/org/mgnl/nicki/dynamic/objects/objects/Person.java
@@ -44,6 +44,9 @@ public class Person extends DynamicTemplateObject {
 	public static final String ATTRIBUTE_FULLNAME = "fullname";
 	public static final String ATTRIBUTE_LANGUAGE = "language";
 	public static final String ATTRIBUTE_MEMBEROF = "memberOf";
+	public static final String ATTRIBUTE_STATUS = "status";
+	public static final String ATTRIBUTE_ASSIGNEDARTICLE = "assignedArticle";
+	public static final String ATTRIBUTE_ATTRIBUTEVALUE = "attributeValue";
 
 	public void initDataModel() {
 		addObjectClass("Person");
@@ -72,6 +75,20 @@ public class Person extends DynamicTemplateObject {
 		
 		dynAttribute = new DynamicReference(Group.class, ATTRIBUTE_MEMBEROF, Config.getProperty("nicki.data.basedn"), 
 				"member", String.class);
+		dynAttribute.setMultiple();
+		addAttribute(dynAttribute);
+
+		dynAttribute = new DynamicAttribute(ATTRIBUTE_STATUS, "nickiStatus",
+				String.class);
+		addAttribute(dynAttribute);
+
+		dynAttribute = new DynamicAttribute(ATTRIBUTE_ASSIGNEDARTICLE, "nickiCatalogArticle",
+				String.class);
+		dynAttribute.setMultiple();
+		addAttribute(dynAttribute);
+
+		dynAttribute = new DynamicAttribute(ATTRIBUTE_ATTRIBUTEVALUE,
+				"nickiCatalogAttribute", String.class);
 		dynAttribute.setMultiple();
 		addAttribute(dynAttribute);
 
@@ -110,6 +127,65 @@ public class Person extends DynamicTemplateObject {
 
 	public String getLanguage() {
 		return (String) get(ATTRIBUTE_LANGUAGE);
+	}
+
+	public static String getActiveFilter() {
+		return "nickiStatus=" + STATUS.ACTIVE.getValue();
+	}
+
+	public static String getInActiveFilter() {
+		return "!(|(nickiStatus=" + STATUS.ACTIVE.getValue() + ")(nickiStatus="
+				+ STATUS.REQUESTED.getValue() + "))";
+	}
+
+	public static String getAllFilter() {
+		return "!(nickiStatus=" + STATUS.REQUESTED.getValue() + ")";
+	}
+
+	public void setStatus(STATUS status) {
+		if(null != status) {
+			put(ATTRIBUTE_STATUS, status.getValue());
+		} else {
+			clear(ATTRIBUTE_STATUS);
+		}
+		
+	}
+
+	public STATUS getStatus() {
+		return STATUS.fromValue(getAttribute(ATTRIBUTE_STATUS));
+	}
+
+	public enum STATUS {
+
+		REQUESTED("beantragt"), INACTIVE("inaktiv-vor-eintritt"), ACTIVE(
+		"aktiv"), DEACTIVATED("deaktiviert"), RESIGNED("ausgetreten"), NOT_SET(
+		"");
+		private final String status;
+
+		private STATUS(String status) {
+			this.status = status;
+		}
+
+		public static STATUS fromValue(String status) {
+			if (INACTIVE.getValue().equals(status)) {
+				return INACTIVE;
+			} else if (ACTIVE.getValue().equals(status)) {
+				return ACTIVE;
+			} else if (DEACTIVATED.getValue().equals(status)) {
+				return DEACTIVATED;
+			} else if (RESIGNED.getValue().equals(status)) {
+				return RESIGNED;
+			} else if (REQUESTED.getValue().equals(status)) {
+				return REQUESTED;
+			}
+
+			return NOT_SET;
+
+		}
+
+		public String getValue() {
+			return status;
+		}
 	}
 
 
---
+++ nicki-dynamic-objects/src/main/java/org/mgnl/nicki/dynamic/objects/shop/Cart.java
@@ -0,0 +1,355 @@
+/**
+ * This file Copyright (c) 2003-2011 Dr. Ralf Hirning
+ * All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the GNU General
+ * Public License and an individual license with Dr. Ralf
+ * Hirning.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or the individual license, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the individual license, this file and the accompanying
+ * materials are made available under the terms of the
+ * individual license.
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package org.mgnl.nicki.dynamic.objects.shop;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+
+import org.mgnl.nicki.dynamic.objects.components.ProcessResult;
+import org.mgnl.nicki.dynamic.objects.objects.Person;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.commons.lang.NotImplementedException;
+import org.w3c.dom.Document;
+import org.mgnl.nicki.core.config.Config;
+import org.mgnl.nicki.core.helper.DataHelper;
+import org.mgnl.nicki.ldap.context.NickiContext;
+import org.mgnl.nicki.ldap.objects.ContextSearchResult;
+import org.mgnl.nicki.ldap.objects.DynamicAttribute;
+import org.mgnl.nicki.ldap.objects.DynamicObject;
+import org.mgnl.nicki.ldap.objects.DynamicObjectException;
+import org.mgnl.nicki.ldap.xml.XmlHelper;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author cna
+ */
+@SuppressWarnings("serial")
+public class Cart extends DynamicObject {
+
+    public enum STATUS {
+
+        NEW,
+        REQUESTED,
+        FINISHED;
+
+        public String getValue() {
+            return this.toString().toLowerCase();
+        }
+
+        public static STATUS fromString(String str) {
+            return STATUS.valueOf(str.toUpperCase());
+        }
+    }
+	
+    private Catalog catalog = null;
+    private HashMap<String, CartEntry> cartentries = new HashMap<String, CartEntry>();
+    private final String ELEM_ATTRIBUTE = "attribute";
+    private final String ELEM_CARTENTRY = "entry";
+    private final String ELEM_CART = "cart";
+    private final String ATTR_CATALOG = "catalog";
+    private final String ATTR_ACTION = "action";
+    private final String ATTR_ID = "id";
+    private final String ATTR_NAME = "name";
+
+    @Override
+    public void initDataModel() {
+        addObjectClass("nickiCart");
+        DynamicAttribute dynAttribute = new DynamicAttribute("name", "cn", String.class);
+        dynAttribute.setNaming();
+        addAttribute(dynAttribute);
+
+        dynAttribute = new DynamicAttribute("data", "nickiData", String.class);
+        addAttribute(dynAttribute);
+
+        dynAttribute = new DynamicAttribute("initiator", "nickiInitiator", String.class);
+        dynAttribute.setForeignKey(Person.class);
+        addAttribute(dynAttribute);
+
+        dynAttribute = new DynamicAttribute("processdate", "nickiProcessDate", String.class);
+        addAttribute(dynAttribute);
+
+        dynAttribute = new DynamicAttribute("processresult", "nickiProcessResult", String.class);
+        addAttribute(dynAttribute);
+
+        dynAttribute = new DynamicAttribute("recipient", "nickiRecipient", String.class);
+        dynAttribute.setForeignKey(Person.class);
+        addAttribute(dynAttribute);
+
+        dynAttribute = new DynamicAttribute("requestdate", "nickiRequestDate", String.class);
+        addAttribute(dynAttribute);
+
+        dynAttribute = new DynamicAttribute("status", "nickiStatus", String.class);
+        addAttribute(dynAttribute);
+        
+		dynAttribute = new DynamicAttribute("statusFlag", "nickiStatusFlag", String.class);
+        dynAttribute.setMultiple();
+		addAttribute(dynAttribute);
+		
+		dynAttribute = new DynamicAttribute("source", "nickiSource", String.class);
+        addAttribute(dynAttribute);
+    }
+
+    @Override
+    public void init(ContextSearchResult rs) throws DynamicObjectException {
+        super.init(rs);
+
+        try {
+            fromXml((String) get("data"));
+        } catch (Exception ex) {
+            System.err.println("xml invalid - " + ex.getMessage());
+            throw new IllegalArgumentException(ex);
+        }
+    }
+
+    public void fromXml(String xml) throws SAXException  {
+
+        Document doc = XmlHelper.getDocumentFromXml(xml);
+
+        Element cart = XmlHelper.selectNode(Element.class, doc, ELEM_CART);
+        catalog = getContext().loadObject(Catalog.class, cart.getAttribute(ATTR_CATALOG));
+        if (catalog == null) {
+            System.err.println("could not load catalog object - id is invalid: \"" + cart.getAttribute(ATTR_CATALOG) + "\"");
+        }
+
+        List<Element> entries = XmlHelper.selectNodes(Element.class, cart, ELEM_CARTENTRY);
+        CartEntry entry;
+
+        for (Element node : entries) {
+            entry = getCartEntryInstance(node);
+
+            cartentries.put(entry.getId(), entry);
+        }
+    }
+
+    public String toXml() throws DynamicObjectException {
+        if (catalog == null) {
+            throw new DynamicObjectException("catalog undefined");
+        }
+
+        Document doc = XmlHelper.getNewDocument();
+
+        Element cart = doc.createElement(ELEM_CART);
+
+        cart.setAttribute(ATTR_CATALOG, catalog.getPath());
+
+        for (String key : cartentries.keySet()) {
+			
+            cart.appendChild(getCartEntryNode(doc, cartentries.get(key)));
+        }
+		
+		doc.appendChild(cart);
+
+        return XmlHelper.getXml(doc);
+
+    }
+	
+	
+
+    @Override
+    public void create() throws DynamicObjectException {
+        put("data", toXml());
+        super.create();
+    }
+
+    @Override
+    public void update() throws DynamicObjectException {
+        put("data", toXml());
+        super.update();
+    }
+
+    public void addProcessResult(ProcessResult result) {
+        throw new NotImplementedException();
+    }
+
+    public ProcessResult getProcessResult(CartEntry entry) {
+        throw new NotImplementedException();
+    }
+
+    public List<ProcessResult> getProcessResults() {
+        throw new NotImplementedException();
+    }
+
+    public void setRequestDate(Date date) {
+        put("requestdate", DataHelper.formatMilli.format(date));
+    }
+
+    public Date getRequestDate() throws ParseException {
+        return DataHelper.formatMilli.parse((String) get("requestdate"));
+    }
+
+    public void setProcessDate(Date date) {
+        put("processdate", DataHelper.formatMilli.format(date));
+    }
+
+    public Date getProcessDate() throws ParseException {
+        return DataHelper.formatMilli.parse((String) get("processdate"));
+    }
+
+    public void setCatalog(Catalog catalog) {
+        this.catalog = catalog;
+    }
+
+    public Catalog getCatalog() {
+        return catalog;
+    }
+
+    public void setStatus(STATUS status) {
+        put("status", status.getValue());
+    }
+	
+	public void setStatusFlag(List<String> statusFlags) {
+		put("statusFlag", statusFlags);
+    }
+	
+	@SuppressWarnings("unchecked")
+	public void addStatusFlag(String statusFlag) {
+		List<String> list;
+		if (get("statusFlag") != null) {
+			list = (List<String>) get("statusFlag");
+		} else {
+			list = new ArrayList<String>();
+		}
+		
+		list.add(statusFlag);
+		put("statusFlag", list);
+    }
+
+    public STATUS getStatus() {
+        return STATUS.fromString((String) get("status"));
+    }
+    
+	@SuppressWarnings("unchecked")
+	public List<String> getStatusFlag() {
+        return (List<String>) get("statusFlag");
+    }
+	
+	public void setSource(String source) {
+        put("source", source);
+    }
+
+    public String getSource() {
+        return (String) get("source");
+    }
+
+    public void addCartEntry(CartEntry entry) {
+        cartentries.put(entry.getId(), entry);
+
+    }
+
+    public CartEntry getCartEntry(String id) {
+        return cartentries.get(id);
+    }
+
+    @SuppressWarnings("unchecked")
+	public Map<String, CartEntry> getCartEntries() {
+        return (Map<String, CartEntry>) cartentries.clone();
+    }
+
+    public void setRecipient(Person person) {
+        put("recipient", person.getPath());
+    }
+
+    public Person getRecipient() {
+        return getContext().loadObject(Person.class, (String) get("recipient"));
+    }
+
+    public void setInitiator(Person person) {
+        put("initiator", person.getPath());
+    }
+
+    public Person getInitiator() {
+        return getContext().loadObject(Person.class, (String) get("initiator"));
+    }
+
+    private CartEntry getCartEntryInstance(Element node) {
+        if (null == node) {
+            return null;
+        }
+
+        CartEntry entry = new CartEntry(
+                node.getAttribute(ATTR_ID),
+                CartEntry.ACTION.valueOf(node.getAttribute(ATTR_ACTION).toUpperCase()));
+
+        for (Element element : XmlHelper.selectNodes(Element.class, node, ELEM_ATTRIBUTE)) {
+            entry.addAttribute(element.getAttribute(ATTR_NAME), element.getTextContent());
+        }
+
+        return entry;
+    }
+
+    private Element getCartEntryNode(Document doc, CartEntry entry) {
+      
+		Element cartentry = doc.createElement(ELEM_CARTENTRY);
+		
+        cartentry.setAttribute(ATTR_ID, entry.getId());
+        cartentry.setAttribute(ATTR_ACTION, entry.getAction().toString().toLowerCase());
+
+        Map<String, String> attributes = entry.getAttributes();
+        Element attr;
+
+        for (String key : attributes.keySet()) {
+            attr = doc.createElement(ELEM_ATTRIBUTE);
+            attr.setAttribute(ATTR_NAME, key);
+            attr.setTextContent(attributes.get(key));
+
+            cartentry.appendChild(attr);
+        }
+
+        return cartentry;
+    }
+
+    public static List<Cart> getAllCarts(NickiContext ctx) {
+        return ctx.loadChildObjects(Cart.class, Config.getProperty("nicki.carts.basedn"), null);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[CART name=");
+        sb.append(getName());
+        sb.append(" catalog=");
+        sb.append((catalog == null) ? "" : catalog.getPath());
+        sb.append(" cartentries.length=");
+        sb.append(cartentries.size());
+        sb.append("]");
+
+
+        return sb.toString();
+    }
+}
