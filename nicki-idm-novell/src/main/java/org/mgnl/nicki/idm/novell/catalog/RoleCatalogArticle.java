4b2f627f726c89bc484cf3e3a7de2f1d7932eb39
rhi <31392706+rhirning@users.noreply.github.com>
2012-03-21 12:41:52 +0000

    CatalogArticles

--- nicki-idm-novell/src/main/java/org/mgnl/nicki/idm/novell/catalog/RoleCatalogArticle.java
+++ nicki-idm-novell/src/main/java/org/mgnl/nicki/idm/novell/catalog/RoleCatalogArticle.java
@@ -1,43 +1,99 @@
 package org.mgnl.nicki.idm.novell.catalog;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
+import org.mgnl.nicki.core.config.Config;
 import org.mgnl.nicki.dynamic.objects.objects.Person;
+import org.mgnl.nicki.dynamic.objects.reference.ReferenceDynamicAttribute;
+import org.mgnl.nicki.dynamic.objects.shop.Catalog;
 import org.mgnl.nicki.dynamic.objects.shop.CatalogArticle;
+import org.mgnl.nicki.idm.novell.extensions.IDMExtension;
 import org.mgnl.nicki.idm.novell.objects.Role;
+import org.mgnl.nicki.ldap.objects.DynamicAttribute;
+import org.mgnl.nicki.ldap.objects.DynamicObjectException;
 
 public class RoleCatalogArticle extends CatalogArticle {
 
 	private static final long serialVersionUID = -5530389061310235734L;
+	public static final String ATTRIBUTE_ROLE = "role";
+	@Override
+	public void initDataModel() {
+		super.initDataModel();
+		
+		DynamicAttribute dynAttribute = new ReferenceDynamicAttribute(Role.class, ATTRIBUTE_ROLE, "nickiRoleRef", String.class,
+				Config.getProperty("nicki.roles.basedn"));
+		dynAttribute.setForeignKey(Role.class);
+		addAttribute(dynAttribute);
+
+	}
+
 
 	@Override
 	public List<CatalogArticle> getArticles(Person person) {
-		// TODO Auto-generated method stub
-		return null;
+		IDMExtension extension;
+		try {
+			extension = person.getContext().getObjectFactory().getDynamicObjectExtension(IDMExtension.class, person);
+			@SuppressWarnings("unchecked")
+			List<CatalogArticle> catalogArticles = (List<CatalogArticle>) extension.get("RoleCatalogArticles");
+			if (catalogArticles == null) {
+				catalogArticles = new ArrayList<CatalogArticle>();
+				List<Role> roles = extension.getRoles();
+				List<CatalogArticle> articles = Catalog.getCatalog().getAllArticles();
+				for (CatalogArticle catalogArticle : articles) {
+					if (RoleCatalogArticle.class.isAssignableFrom(catalogArticle.getClass())) {
+						RoleCatalogArticle roleCatalogArticle = (RoleCatalogArticle) catalogArticle;
+						for (Role role : roles) {
+							if (roleCatalogArticle.contains(role)) {
+								catalogArticles.add(roleCatalogArticle);
+							}
+						}
+					}
+				}
+				extension.put("RoleCatalogArticles", catalogArticles);
+			}
+			return catalogArticles;
+		} catch (DynamicObjectException e) {
+			e.printStackTrace();
+		}
+		return new ArrayList<CatalogArticle>();
 	}
+
+	public boolean contains(Role role) {
+		if (StringUtils.equalsIgnoreCase(role.getPath(), (String) get(ATTRIBUTE_ROLE))) {
+			return true;
+		}
+		return false;
+	}
+
 
 	@Override
 	public Date getStart(Person person) {
-		// TODO Auto-generated method stub
+		Role role = getRole();
+		if (role != null) {
+			return role.getStartTime();
+		}
 		return null;
 	}
 
 	@Override
 	public Date getEnd(Person person) {
-		// TODO Auto-generated method stub
+		Role role = getRole();
+		if (role != null) {
+			return role.getEndTime();
+		}
 		return null;
 	}
 
 	@Override
 	public String getSpecifier(Person person) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public Role getRole() {
-		// TODO Auto-generated method stub
-		return null;
+		return getForeignKeyObject(Role.class, ATTRIBUTE_ROLE);
 	}
 
 }
