e13ea87aea7032b7177a66123509da008adb7cce
rhi <31392706+rhirning@users.noreply.github.com>
2012-07-18 17:12:28 +0000

    [maven-release-plugin]  copy for tag nicki-parent-1.70

---
+++ nicki-parent-1.70/nicki-dynamic-objects/src/main/java/org/mgnl/nicki/dynamic/objects/shop/CatalogArticleAttribute.java
@@ -0,0 +1,100 @@
+/**
+ * This file Copyright (c) 2003-2011 Dr. Ralf Hirning
+ * All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the GNU General
+ * Public License and an individual license with Dr. Ralf
+ * Hirning.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or the individual license, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the individual license, this file and the accompanying
+ * materials are made available under the terms of the
+ * individual license.
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package org.mgnl.nicki.dynamic.objects.shop;
+
+import java.io.Serializable;
+
+import org.jdom.Element;
+import org.mgnl.nicki.core.i18n.I18n;
+
+@SuppressWarnings("serial")
+public class CatalogArticleAttribute implements Serializable{
+	public static final String SEPARATOR = "|"; 
+	private String name;
+	private String key;
+	private String type;
+	private String contentClass;
+	public CatalogArticleAttribute(String name, String key, String type) {
+		this.name = name;
+		this.key = key;
+		this.type = type;
+	}
+
+	public CatalogArticleAttribute(Element attributeElement) {
+		this.name = attributeElement.getAttributeValue("name");
+		this.key = attributeElement.getAttributeValue("label");
+		this.type = attributeElement.getAttributeValue("type");
+		this.contentClass = attributeElement.getAttributeValue("contentClass");
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLabel() {
+		return I18n.getText(key);
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public String getAttributeString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append(name).append(SEPARATOR).append(key).append(SEPARATOR).append(type);
+		return sb.toString();
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	public String getKey() {
+		return key;
+	}
+	
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("[catalogArticleAttribute name='").append(getName());
+		sb.append("' label='").append(getLabel());
+		sb.append("' key='").append(getKey());
+		sb.append("' type='").append(getType());
+		sb.append("']\n");
+		return sb.toString();
+	}
+
+	public String getContentClass() {
+		return contentClass;
+	}
+}
---
+++ nicki-parent-1.70/nicki-ldap/src/main/java/org/mgnl/nicki/ldap/auth/NickiLoginModule.java
@@ -0,0 +1,162 @@
+/**
+ * This file Copyright (c) 2003-2011 Dr. Ralf Hirning
+ * All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the GNU General
+ * Public License and an individual license with Dr. Ralf
+ * Hirning.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or the individual license, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the individual license, this file and the accompanying
+ * materials are made available under the terms of the
+ * individual license.
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package org.mgnl.nicki.ldap.auth;
+
+import java.util.Map;
+
+import javax.security.auth.Subject;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.login.LoginException;
+import javax.security.auth.spi.LoginModule;
+
+
+public abstract class NickiLoginModule implements LoginModule {
+
+	// initial state
+	private Subject subject;
+	private CallbackHandler callbackHandler;
+	private Map<String, ?> sharedState;
+	private Map<String, ?> options;
+
+	// configurable option
+	private boolean debug = false;
+
+	// the authentication status
+	private boolean succeeded = false;
+	private boolean commitSucceeded = false;
+
+	// username and password
+	private String username;
+	private String password;
+	
+	// Principal
+	private NickiPrincipal userPrincipal;
+	
+	protected NickiLoginModule() {
+		
+	}
+
+	public abstract boolean login() throws LoginException;
+
+	public void initialize(Subject subject, CallbackHandler callbackHandler,
+			Map<String, ?> sharedState, Map<String, ?> options) {
+
+
+		this.subject = subject;
+		this.callbackHandler = callbackHandler;
+		this.sharedState = sharedState;
+		this.options = options;
+
+		// initialize any configured options
+		debug = "true".equalsIgnoreCase((String) options.get("debug"));
+	}
+
+	public boolean commit() throws LoginException {
+		if (succeeded == false) {
+			return false;
+		} else {
+			// add a Principal (authenticated identity)
+			// to the Subject
+
+			// assume the user we authenticated is the SamplePrincipal
+			try {
+				userPrincipal = new NickiPrincipal(username, password);
+			} catch (InvalidPrincipalException e) {
+				throw new LoginException(e.getMessage());
+			}
+			if (!subject.getPrincipals().contains(userPrincipal)) {
+				subject.getPrincipals().add(userPrincipal);
+			}
+
+			// in any case, clean out state
+			username = null;
+
+			commitSucceeded = true;
+			return true;
+		}
+	}
+
+	public boolean abort() throws LoginException {
+		if (succeeded == false) {
+			return false;
+		} else if (succeeded == true && commitSucceeded == false) {
+			// login succeeded but overall authentication failed
+			succeeded = false;
+			username = null;
+			userPrincipal = null;
+		} else {
+			// overall authentication succeeded and commit succeeded,
+			// but someone else's commit failed
+			logout();
+		}
+		return true;
+	}
+
+	public boolean logout() throws LoginException {
+		subject.getPrincipals().remove(userPrincipal);
+		succeeded = false;
+		succeeded = commitSucceeded;
+		username = null;
+		userPrincipal = null;
+		return true;
+	}
+
+	protected CallbackHandler getCallbackHandler() {
+		return callbackHandler;
+	}
+
+	protected void setUsername(String username) {
+		this.username = username;
+	}
+
+	protected void setSucceeded(boolean succeeded) {
+		this.succeeded = succeeded;
+	}
+
+	protected void setPassword(String password) {
+		this.password = password;
+	}
+
+	protected Map<String, ?> getSharedState() {
+		return sharedState;
+	}
+
+	protected Map<String, ?> getOptions() {
+		return options;
+	}
+
+	protected boolean isDebug() {
+		return debug;
+	}
+
+}
