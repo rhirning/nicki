9b1eefa663162ca22a889c538a8dad7c750dfbeb
rhi <rhi@92c39532-7ad5-11e0-8644-75d2b71920e0>
2012-07-19 07:17:03 +0000

    [maven-release-plugin]  copy for tag nicki-parent-1.71

---
+++ nicki/tags/nicki-parent/nicki-parent-1.71/nicki-idm-novell/src/main/java/org/mgnl/nicki/idm/novell/catalog/RoleCatalogArticle.java
@@ -0,0 +1,122 @@
+/**
+ * This file Copyright (c) 2003-2011 Dr. Ralf Hirning
+ * All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the GNU General
+ * Public License and an individual license with Dr. Ralf
+ * Hirning.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or the individual license, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the individual license, this file and the accompanying
+ * materials are made available under the terms of the
+ * individual license.
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package org.mgnl.nicki.idm.novell.catalog;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.mgnl.nicki.core.config.Config;
+import org.mgnl.nicki.dynamic.objects.objects.Person;
+import org.mgnl.nicki.dynamic.objects.reference.ReferenceDynamicAttribute;
+import org.mgnl.nicki.dynamic.objects.shop.Catalog;
+import org.mgnl.nicki.dynamic.objects.shop.CatalogArticle;
+import org.mgnl.nicki.idm.novell.objects.IDMPerson;
+import org.mgnl.nicki.idm.novell.objects.Role;
+import org.mgnl.nicki.ldap.objects.DynamicAttribute;
+
+public class RoleCatalogArticle extends CatalogArticle {
+
+	private static final long serialVersionUID = -5530389061310235734L;
+	public static final String ATTRIBUTE_ROLE = "role";
+	private List<CatalogArticle> catalogArticles = null;;
+	@Override
+	public void initDataModel() {
+		super.initDataModel();
+		addObjectClass("nickiRoleArticle");
+
+		DynamicAttribute dynAttribute = new ReferenceDynamicAttribute(Role.class, ATTRIBUTE_ROLE, "nickiRoleRef", String.class,
+				Config.getProperty("nicki.roles.basedn"));
+		dynAttribute.setForeignKey(Role.class);
+		addAttribute(dynAttribute);
+
+	}
+
+
+	@Override
+	public List<CatalogArticle> getArticles(Person person) {
+		if (catalogArticles == null) {
+			catalogArticles = new ArrayList<CatalogArticle>();
+			List<Role> roles = ((IDMPerson)person).getRoles();
+			List<CatalogArticle> articles = Catalog.getCatalog().getAllArticles();
+			for (CatalogArticle catalogArticle : articles) {
+				if (RoleCatalogArticle.class.isAssignableFrom(catalogArticle.getClass())) {
+					RoleCatalogArticle roleCatalogArticle = (RoleCatalogArticle) catalogArticle;
+					for (Role role : roles) {
+						if (roleCatalogArticle.contains(role)) {
+							catalogArticles.add(roleCatalogArticle);
+						}
+					}
+				}
+			}
+		}
+		return catalogArticles;
+	}
+
+	public boolean contains(Role role) {
+		if (StringUtils.equalsIgnoreCase(role.getPath(), (String) get(ATTRIBUTE_ROLE))) {
+			return true;
+		}
+		return false;
+	}
+
+
+	@Override
+	public Date getStart(Person person) {
+		Role role = getRole();
+		if (role != null) {
+			return role.getStartTime();
+		}
+		return null;
+	}
+
+	@Override
+	public Date getEnd(Person person) {
+		Role role = getRole();
+		if (role != null) {
+			return role.getEndTime();
+		}
+		return null;
+	}
+
+	@Override
+	public String getSpecifier(Person person) {
+		return null;
+	}
+
+	public Role getRole() {
+		return getForeignKeyObject(Role.class, ATTRIBUTE_ROLE);
+	}
+
+}
---
+++ nicki/tags/nicki-parent/nicki-parent-1.71/nicki-vaadin-base/src/main/java/org/mgnl/nicki/vaadin/base/fields/AttributeTextAreaField.java
@@ -0,0 +1,89 @@
+/**
+ * This file Copyright (c) 2003-2011 Dr. Ralf Hirning
+ * All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the GNU General
+ * Public License and an individual license with Dr. Ralf
+ * Hirning.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or the individual license, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the individual license, this file and the accompanying
+ * materials are made available under the terms of the
+ * individual license.
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package org.mgnl.nicki.vaadin.base.fields;
+
+import java.io.Serializable;
+
+import org.mgnl.nicki.ldap.objects.DynamicObject;
+import org.mgnl.nicki.vaadin.base.data.AttributeDataContainer;
+import org.mgnl.nicki.vaadin.base.data.DataContainer;
+import org.mgnl.nicki.vaadin.base.editor.DynamicObjectValueChangeListener;
+import org.mgnl.nicki.vaadin.base.listener.AttributeInputListener;
+
+import com.vaadin.ui.AbsoluteLayout;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.ComponentContainer;
+import com.vaadin.ui.TextArea;
+
+@SuppressWarnings("serial")
+public class AttributeTextAreaField extends BaseDynamicAttributeField implements DynamicAttributeField, Serializable {
+
+	private AbsoluteLayout mainLayout;
+
+	private TextArea field;
+	private DataContainer property;
+	public void init(String attributeName, DynamicObject dynamicObject, DynamicObjectValueChangeListener objectListener) {
+
+		// common part: create layout
+		mainLayout = new AbsoluteLayout();
+		
+		// top-level component properties
+		mainLayout.setWidth("500px");
+		mainLayout.setHeight("120px");
+
+		property = new AttributeDataContainer(dynamicObject, attributeName);
+		field = new TextArea(getName(dynamicObject, attributeName));
+		field.setWidth("400px");
+		field.setHeight("100px");
+		field.setValue(property.getValue());
+		field.setImmediate(false);
+		field.addListener(new AttributeInputListener(property, objectListener));
+		mainLayout.addComponent(field, "top:20.0px;left:0.0px;");
+		
+		// editButton
+		Button editButton = new Button();
+		editButton.setWidth("-1px");
+		editButton.setHeight("-1px");
+		editButton.setCaption("Edit");
+		editButton.setImmediate(false);
+//		mainLayout.addComponent(editButton, "top:20.0px;left:620.0px;");
+
+		
+	}
+
+	public ComponentContainer getComponent(boolean readOnly) {
+		field.setReadOnly(readOnly);
+		return mainLayout;
+	}
+	
+}
