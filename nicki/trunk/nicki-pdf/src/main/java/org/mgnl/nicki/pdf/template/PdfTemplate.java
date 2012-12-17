4bba899794863468f83127b6fb74b55ac6562a83
cnaber <cnaber@92c39532-7ad5-11e0-8644-75d2b71920e0>
2012-12-17 13:46:53 +0000

    last update

--- nicki/trunk/nicki-pdf/src/main/java/org/mgnl/nicki/pdf/template/PdfTemplate.java
+++ nicki/trunk/nicki-pdf/src/main/java/org/mgnl/nicki/pdf/template/PdfTemplate.java
@@ -9,56 +9,42 @@ import java.io.InputStream;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import org.mgnl.nicki.pdf.model.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PdfTemplate {
 
-	private Pdf pdf = null;
-	public PdfTemplate(String templatePath) {
-		try {
-			pdf = unmarshal(Pdf.class, templatePath);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (JAXBException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	private static final Logger log = LoggerFactory.getLogger(PdfTemplate.class);
+	private Document document = null;
+
+	public PdfTemplate(String templateContent) throws JAXBException {
+		document = unmarshal(Document.class, templateContent);
 	}
 
-
-    
-    private Object unmarshal(ByteArrayOutputStream outputStream, Class<?> rootClazz) throws JAXBException  {
-		ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
-		return unmarshal(rootClazz, inputStream);
-    }
-    
-    
-    private <T> T unmarshal(Class<T> docClass, String path) throws JAXBException, FileNotFoundException {
-
-		FileInputStream file = new FileInputStream(path);
-		return unmarshal(docClass, file);
-    }
-    
-    public <T> T unmarshal( Class<T> docClass, InputStream inputStream )
-    	    throws JAXBException {
-    	    String packageName = docClass.getPackage().getName();
-    	    JAXBContext jc = JAXBContext.newInstance( packageName );
-    	    Unmarshaller u = jc.createUnmarshaller();
-    	    @SuppressWarnings("unchecked")
-			T doc = (T)u.unmarshal( inputStream );
-    	    return doc;
-    	}
-
-
-
-	public Pdf getPdf() {
-		return pdf;
+	public PdfTemplate(InputStream inputStream) throws JAXBException {
+		document = unmarshal(Document.class, inputStream);
 	}
 
-
-
-	public void setPdf(Pdf pdf) {
-		this.pdf = pdf;
+	public <T> T unmarshal(Class<T> docClass, String templateContent) throws JAXBException {
+		return unmarshal(docClass, new ByteArrayInputStream(templateContent.getBytes()));
 	}
 
+	public <T> T unmarshal(Class<T> docClass, InputStream inputStream)
+			throws JAXBException {
+		String packageName = docClass.getPackage().getName();
+		JAXBContext jc = JAXBContext.newInstance(packageName);
+		Unmarshaller u = jc.createUnmarshaller();
+		@SuppressWarnings("unchecked")
+		T doc = (T) u.unmarshal(inputStream);
+		return doc;
+	}
+
+	public Document getDocument() {
+		return document;
+	}
+
+	public void setDocument(Document document) {
+		this.document = document;
+	}
 }
