aa9f4542e68803bc100cdb3d7407e73dc2e4c5d8
rhirning <rhirning@92c39532-7ad5-11e0-8644-75d2b71920e0>
2013-07-15 07:06:24 +0000

    [maven-release-plugin]  copy for tag nicki-parent-2.06

---
+++ nicki/tags/nicki-parent/nicki-parent-2.06/nicki-vaadin-base/src/main/java/org/mgnl/nicki/vaadin/base/editor/ExportStreamSource.java
@@ -0,0 +1,72 @@
+/**
+ * This file Copyright (c) 2003-2011 Dr. Ralf Hirning
+ * All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the GNU General
+ * Public License and an individual license with Dr. Ralf
+ * Hirning.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or the individual license, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the individual license, this file and the accompanying
+ * materials are made available under the terms of the
+ * individual license.
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package org.mgnl.nicki.vaadin.base.editor;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import org.mgnl.nicki.core.context.NickiContext;
+import org.mgnl.nicki.core.objects.DynamicObject;
+import org.mgnl.nicki.core.util.XMLBuilder;
+
+import com.vaadin.terminal.StreamResource.StreamSource;
+
+public class ExportStreamSource implements StreamSource{
+
+	private static final long serialVersionUID = -8068031351212191141L;
+	private DynamicObject dynamicObject;
+	private NickiContext context;
+	private boolean selfOnly;
+	
+	public ExportStreamSource(DynamicObject dynamicObject, NickiContext context, boolean selfOnly) {
+		this.dynamicObject = dynamicObject;
+		this.context = context;
+		this.selfOnly = selfOnly;
+	}
+
+	public InputStream getStream() {
+		try {
+			XMLBuilder builder = new XMLBuilder(this.context, this.dynamicObject.getPath(), selfOnly);
+			Writer writer = new StringWriter();
+			builder.write(writer);
+			
+			return new ByteArrayInputStream(writer.toString().getBytes("UTF-8"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
