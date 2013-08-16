318b3de682b93b0c70a159eb5eaf7e13690d5198
rhirning <31392706+rhirning@users.noreply.github.com>
2013-08-16 05:54:50 +0000

    [maven-release-plugin]  copy for tag nicki-parent-3.01

---
+++ nicki-parent-3.01/nicki-idm-novell/src/main/java/org/mgnl/nicki/idm/novell/catalog/ResourceCatalogArticle.java
@@ -0,0 +1,97 @@
+/**
+ * This file Copyright (c) 2003-2011 Dr. Ralf Hirning
+ * All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the GNU General
+ * Public License and an individual license with Dr. Ralf
+ * Hirning.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or the individual license, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the individual license, this file and the accompanying
+ * materials are made available under the terms of the
+ * individual license.
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package org.mgnl.nicki.idm.novell.catalog;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.mgnl.nicki.core.annotation.DynamicObject;
+import org.mgnl.nicki.core.annotation.DynamicReferenceAttribute;
+import org.mgnl.nicki.core.annotation.ObjectClass;
+import org.mgnl.nicki.dynamic.objects.objects.Person;
+import org.mgnl.nicki.idm.novell.objects.IdmPerson;
+import org.mgnl.nicki.idm.novell.objects.Resource;
+import org.mgnl.nicki.shop.inventory.InventoryArticle;
+import org.mgnl.nicki.shop.inventory.InventoryAttribute;
+import org.mgnl.nicki.shop.objects.Catalog;
+import org.mgnl.nicki.shop.objects.CatalogArticle;
+
+@DynamicObject
+@ObjectClass("nickiResourceArticle")
+public class ResourceCatalogArticle extends CatalogArticle {
+
+	private static final long serialVersionUID = 3397169876567940029L;
+
+	public static final String ATTRIBUTE_RESOURCE = "resource";
+	
+	@DynamicReferenceAttribute(externalName="nickiResourceRef", foreignKey=Resource.class, reference=Resource.class,
+			baseProperty="nicki.resources.basedn")
+	private String resource;
+
+	@Override
+	public List<InventoryArticle> getInventoryArticles(Person person) {
+		List<InventoryArticle> inventoryArticles = new ArrayList<InventoryArticle>();
+		List<Resource> resources = ((IdmPerson)person).getResources();
+		List<CatalogArticle> articles = Catalog.getCatalog().getAllArticles();
+		for (CatalogArticle catalogArticle : articles) {
+			if (ResourceCatalogArticle.class.isAssignableFrom(catalogArticle.getClass())) {
+				ResourceCatalogArticle resourceCatalogArticle = (ResourceCatalogArticle) catalogArticle;
+				for (Resource resource : resources) {
+					if (resourceCatalogArticle.contains(resource)) {
+						String articleId = catalogArticle.getCatalogPath();
+						String specifier = resource.getParameter();
+						Map<String, String> attributeMap = person.getCatalogAttributes(articleId, specifier);
+						List<InventoryAttribute> attributes = getInventoryAttributes(catalogArticle, attributeMap);
+						inventoryArticles.add(new InventoryArticle(catalogArticle, resource.getStartTime(),
+								resource.getEndTime(), attributes));
+					}
+				}
+			}
+		}
+		return inventoryArticles;
+	}
+
+	public boolean contains(Resource resource) {
+		if (getResource() != null && StringUtils.equalsIgnoreCase(resource.getPath(), getResource().getPath())) {
+			return true;
+		}
+		return false;
+	}
+
+	public Resource getResource() {
+		return getForeignKeyObject(Resource.class, ATTRIBUTE_RESOURCE);
+	}
+	
+}
---
+++ nicki-parent-3.01/nicki-shop/src/main/java/org/mgnl/nicki/shop/renderer/CheckBoxChangeListener.java
@@ -0,0 +1,43 @@
+package org.mgnl.nicki.shop.renderer;
+
+import org.mgnl.nicki.core.helper.DataHelper;
+import org.mgnl.nicki.shop.inventory.Inventory;
+import org.mgnl.nicki.shop.inventory.InventoryArticle;
+import org.mgnl.nicki.shop.objects.CatalogArticle;
+
+import com.vaadin.data.Item;
+import com.vaadin.data.Property;
+import com.vaadin.data.Property.ValueChangeEvent;
+
+public class CheckBoxChangeListener implements Property.ValueChangeListener {
+	private static final long serialVersionUID = 5397184007544830202L;
+	private Inventory inventory;
+	private CatalogArticle catalogArticle;
+	private TableRenderer tableRenderer;
+	public CheckBoxChangeListener(Inventory inventory,
+			CatalogArticle catalogArticle, TableRenderer tableRenderer) {
+		super();
+		this.inventory = inventory;
+		this.catalogArticle = catalogArticle;
+		this.tableRenderer = tableRenderer;
+	}
+
+
+
+
+	@Override
+	public void valueChange(ValueChangeEvent event) {
+		Item item = tableRenderer.getTable().getItem(catalogArticle);
+		String checkedString = String.valueOf(event.getProperty().getValue());
+		boolean checked = DataHelper.booleanOf(checkedString);
+		  if (checked) {
+			  InventoryArticle inventoryArticle = inventory.addArticle(catalogArticle);
+			  tableRenderer.showEntry(item, catalogArticle, inventoryArticle);
+		  } else {
+			  inventory.removeArticle(catalogArticle);
+			  tableRenderer.hideEntry(item);
+		  }
+
+	}
+	
+}
