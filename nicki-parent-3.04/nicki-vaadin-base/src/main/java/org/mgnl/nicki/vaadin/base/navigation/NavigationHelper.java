d675efc109c204b8768e5e9e342e5d7febf1d0b6
rhirning <31392706+rhirning@users.noreply.github.com>
2013-08-20 12:35:33 +0000

    [maven-release-plugin]  copy for tag nicki-parent-3.04

---
+++ nicki-parent-3.04/nicki-vaadin-base/src/main/java/org/mgnl/nicki/vaadin/base/navigation/NavigationHelper.java
@@ -0,0 +1,53 @@
+package org.mgnl.nicki.vaadin.base.navigation;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+
+public class NavigationHelper {
+
+	public static void executeCommands(Object object, NavigationCommand command) {
+		List<NavigationCommandEntry> toBeRemoved = new ArrayList<NavigationCommandEntry>();
+		for (NavigationCommandEntry commandEntry : command.getEntries()) {
+			if (canExecute(object, commandEntry)) {
+				try {
+					execute(object, commandEntry);
+					toBeRemoved.add(commandEntry);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				continue;
+			}
+		}
+		
+		for (NavigationCommandEntry navigationCommandEntry : toBeRemoved) {
+			command.remove(navigationCommandEntry);
+		}
+	}
+
+	private static boolean canExecute(Object object,
+			NavigationCommandEntry commandEntry) {
+		for (Method method : object.getClass().getDeclaredMethods()) {
+			Command command = method.getAnnotation(Command.class);
+			if (command != null && StringUtils.equals(command.name(), commandEntry.getCommandName())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static void execute(Object object,
+			NavigationCommandEntry commandEntry) throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+		for (Method method : object.getClass().getDeclaredMethods()) {
+			Command command = method.getAnnotation(Command.class);
+			if (command != null && StringUtils.equals(command.name(), commandEntry.getCommandName())) {
+				method.invoke(object, commandEntry.getData());
+			}
+		}
+		
+	}
+
+}
