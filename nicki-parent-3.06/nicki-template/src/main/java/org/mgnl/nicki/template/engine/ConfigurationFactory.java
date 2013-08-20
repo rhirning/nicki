8cec829f165bf64c7f2daf6191e1a7b8a35db021
rhirning <31392706+rhirning@users.noreply.github.com>
2013-08-20 14:10:06 +0000

    [maven-release-plugin]  copy for tag nicki-parent-3.06

---
+++ nicki-parent-3.06/nicki-template/src/main/java/org/mgnl/nicki/template/engine/ConfigurationFactory.java
@@ -0,0 +1,63 @@
+/**
+ * This file Copyright (c) 2003-2011 Dr. Ralf Hirning
+ * All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the GNU General
+ * Public License and an individual license with Dr. Ralf
+ * Hirning.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or the individual license, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the individual license, this file and the accompanying
+ * materials are made available under the terms of the
+ * individual license.
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package org.mgnl.nicki.template.engine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.mgnl.nicki.core.auth.InvalidPrincipalException;
+import org.mgnl.nicki.core.context.AppContext;
+import org.mgnl.nicki.template.loader.JndiTemplateLoader;
+
+import freemarker.template.Configuration;
+import freemarker.template.ObjectWrapper;
+
+public class ConfigurationFactory {
+
+	private static ConfigurationFactory instance = new ConfigurationFactory();
+	private Map<String, Configuration> configurations = new HashMap<String, Configuration>();
+	public Configuration getConfiguration(String baseDn) throws InvalidPrincipalException {
+		if (!configurations.containsKey(baseDn)) {
+			JndiTemplateLoader loader = new JndiTemplateLoader(AppContext.getSystemContext(), baseDn);
+			Configuration cfg = new Configuration();
+			cfg.setTemplateLoader(loader);
+			cfg.setObjectWrapper(ObjectWrapper.BEANS_WRAPPER);
+			configurations.put(baseDn, cfg);
+		}
+		return configurations.get(baseDn);
+	}
+
+	public static ConfigurationFactory getInstance() {
+		return instance;
+	}
+}
---
+++ nicki-parent-3.06/nicki-vaadin-base/src/main/java/org/mgnl/nicki/vaadin/base/editor/TreeDropHandler.java
@@ -0,0 +1,94 @@
+/**
+ * This file Copyright (c) 2003-2011 Dr. Ralf Hirning
+ * All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the GNU General
+ * Public License and an individual license with Dr. Ralf
+ * Hirning.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or the individual license, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the individual license, this file and the accompanying
+ * materials are made available under the terms of the
+ * individual license.
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package org.mgnl.nicki.vaadin.base.editor;
+
+import org.mgnl.nicki.core.objects.DynamicObject;
+
+import com.vaadin.event.DataBoundTransferable;
+import com.vaadin.event.dd.DragAndDropEvent;
+import com.vaadin.event.dd.DropHandler;
+import com.vaadin.event.dd.acceptcriteria.AcceptAll;
+import com.vaadin.event.dd.acceptcriteria.AcceptCriterion;
+import com.vaadin.shared.ui.dd.VerticalDropLocation;
+import com.vaadin.ui.AbstractSelect.AbstractSelectTargetDetails;
+import com.vaadin.ui.Notification;
+
+@SuppressWarnings("serial")
+public class TreeDropHandler implements DropHandler {
+	private TreeEditor editor;
+
+	public TreeDropHandler(TreeEditor editor) {
+		super();
+		this.editor = editor;
+	}
+
+	public void drop(DragAndDropEvent dropEvent) {
+
+        try {
+			DataBoundTransferable t = (DataBoundTransferable) dropEvent.getTransferable();
+//			Container sourceContainer = t.getSourceContainer();
+			DynamicObject sourceItemId = (DynamicObject) t.getItemId();
+//			Item sourceItem = sourceContainer.getItem(sourceItemId);
+
+			AbstractSelectTargetDetails dropData = ((AbstractSelectTargetDetails) dropEvent
+			.getTargetDetails());
+			DynamicObject targetItemId = (DynamicObject) dropData.getItemIdOver();
+			dropData.getDropLocation(); // TODO
+			
+			Class<? extends DynamicObject> sourceClass = sourceItemId.getClass();
+			Class<? extends DynamicObject> targetClass = targetItemId.getClass();
+			if (!editor.getAllowedChildren(targetClass).contains(sourceClass)
+					|| !(dropData.getDropLocation()== VerticalDropLocation.MIDDLE)
+					|| editor.isParent(sourceItemId, targetItemId)
+			) {
+			    String errorMessage = "Bad target";
+				Notification.show(errorMessage, Notification.Type.WARNING_MESSAGE);
+				return;
+			}
+			
+			editor.moveObject(sourceItemId, targetItemId);
+
+//			String debugMessage = "source: " + sourceItemId.getName() + ", target: " +  targetItemId.getName();
+			//editor.getWindow().showNotification(debugMessage, Notification.TYPE_WARNING_MESSAGE);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+	}
+
+	public AcceptCriterion getAcceptCriterion() {
+		return AcceptAll.get();
+	}
+
+}
