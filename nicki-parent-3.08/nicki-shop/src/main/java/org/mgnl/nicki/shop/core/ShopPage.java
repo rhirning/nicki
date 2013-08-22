4c5dce0dbd4ae51205daa83822527a55b7a52279
rhirning <31392706+rhirning@users.noreply.github.com>
2013-08-22 08:35:22 +0000

    [maven-release-plugin]  copy for tag nicki-parent-3.08

---
+++ nicki-parent-3.08/nicki-shop/src/main/java/org/mgnl/nicki/shop/core/ShopPage.java
@@ -0,0 +1,256 @@
+/**
+ * This file Copyright (c) 2003-2011 Dr. Ralf Hirning
+ * All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the GNU General
+ * Public License and an individual license with Dr. Ralf
+ * Hirning.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or the individual license, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the individual license, this file and the accompanying
+ * materials are made available under the terms of the
+ * individual license.
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package org.mgnl.nicki.shop.core;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.jdom.Element;
+import org.mgnl.nicki.core.i18n.I18n;
+import org.mgnl.nicki.core.util.Classes;
+import org.mgnl.nicki.shop.base.objects.Catalog;
+import org.mgnl.nicki.shop.base.objects.CatalogArticle;
+import org.mgnl.nicki.shop.base.objects.CatalogArticleAttribute;
+
+@SuppressWarnings("serial")
+public class ShopPage implements ShopViewerComponent, Serializable{
+	private Shop shop;
+	private TYPE type;
+	private String name;
+	private String label;
+	private String referencedPage;
+	private List<ShopPage> pageList = new ArrayList<ShopPage>();
+	private List<CatalogArticle> articleList = new ArrayList<CatalogArticle>();
+	private List<CatalogArticleAttribute> attributeList = new ArrayList<CatalogArticleAttribute>();
+	private String renderer;
+	private String selectorClass;
+
+	public ShopPage(Shop shop, Element pageElement) {
+		this.shop = shop;
+		this.type = getPageType(pageElement);
+		
+		if (type == TYPE.SHOP_ARTICLE_PAGE) {
+			String catalogArticleId = pageElement.getAttributeValue("article");
+			if (StringUtils.isNotEmpty(catalogArticleId)) {
+				CatalogArticle article = this.shop.getArticle(catalogArticleId);
+				this.name = article.getName();
+				this.label = article.getDisplayName();
+				this.articleList.add(article);
+				this.renderer = pageElement.getAttributeValue("renderer");
+			}
+		} else if (type == TYPE.STRUCTURE_PAGE) {
+			this.renderer = pageElement.getAttributeValue("renderer");
+			this.name = pageElement.getAttributeValue("name");
+			this.label = I18n.getText(pageElement.getAttributeValue("label"));
+			@SuppressWarnings("unchecked")
+			List<Element> pages = pageElement.getChildren("page");
+			if (pages != null && pages.size() > 0) {
+				for (Element pElement : pages) {
+					this.pageList.add(new ShopPage(shop, pElement));
+				}
+			}
+			@SuppressWarnings("unchecked")
+			List<Element> articles = pageElement.getChildren("article");
+			if (articles != null && articles.size() > 0) {
+				for (Element articleElement : articles) {
+					String catalogArticleId = articleElement.getAttributeValue("article");
+					if (StringUtils.isNotEmpty(catalogArticleId)) {
+						CatalogArticle article = this.shop.getArticle(catalogArticleId);
+						this.articleList.add(article);
+					}
+				}
+			}
+		} else if (type == TYPE.PAGE_REF_PAGE) {
+			this.renderer = pageElement.getAttributeValue("renderer");
+			this.name = pageElement.getAttributeValue("name");
+			this.label = I18n.getText(pageElement.getAttributeValue("label"));
+			this.referencedPage = pageElement.getAttributeValue("page");
+			this.articleList.addAll(Catalog.getCatalog().getReferencedPage(referencedPage).getArticles());
+			@SuppressWarnings("unchecked")
+			List<Element> articles = pageElement.getChildren("article");
+			if (articles != null && articles.size() > 0) {
+				for (Element articleElement : articles) {
+					String catalogArticleId = articleElement.getAttributeValue("article");
+					if (StringUtils.isNotEmpty(catalogArticleId)) {
+						CatalogArticle article = this.shop.getArticle(catalogArticleId);
+						this.articleList.add(article);
+					}
+				}
+			}
+		} else if (type == TYPE.ARTICLE_SELECT) {
+			this.renderer = pageElement.getAttributeValue("renderer");
+			this.name = pageElement.getAttributeValue("name");
+			this.label = I18n.getText(pageElement.getAttributeValue("label"));
+			this.selectorClass = pageElement.getAttributeValue("selector");
+			try {
+				CatalogArticleSelector selector = (CatalogArticleSelector) Classes.newInstance(selectorClass);
+				selector.setShopper(shop.getShopper());
+				selector.setRecipient(shop.getRecipient());
+				List<CatalogArticle> articles = selector.getArticles();
+				if (articles != null && articles.size() > 0) {
+					this.articleList.addAll(articles);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private TYPE getPageType(Element pageElement) {
+		String typeName = pageElement.getAttributeValue("type");
+		if (StringUtils.isNotEmpty(typeName)) {
+			return TYPE.getType(typeName);
+		}
+		String article = pageElement.getAttributeValue("article"); 
+		if (StringUtils.isNotEmpty(article)) {
+			return TYPE.SHOP_ARTICLE_PAGE;
+		}
+		return TYPE.STRUCTURE_PAGE;
+		
+	}
+
+	public enum TYPE {
+		SHOP_ARTICLE_PAGE("shopArticlePage"),
+		STRUCTURE_PAGE("structurePage"),
+		PAGE_REF_PAGE("pageRef"),
+		ARTICLE_SELECT("select");
+
+		String name;
+		TYPE(String name) {
+			this.name = name;
+		}
+		public static TYPE getType(String name) {
+			if ("shopArticlePage".equals(name)) {
+				return SHOP_ARTICLE_PAGE;
+			} else if ("structurePage".equals(name)) {
+				return STRUCTURE_PAGE;
+			} else if ("pageRef".equals(name)) {
+				return PAGE_REF_PAGE;
+			} else if ("select".equals(name)) {
+				return ARTICLE_SELECT;
+			} else {
+				return null;
+			}
+		}
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public String getLabel() {
+		return label;
+	}
+
+	public String getRenderer() {
+		return renderer;
+	}
+
+	public TYPE getType() {
+		return type;
+	}
+
+	public boolean hasAttributes() {
+		return this.attributeList != null && this.attributeList.size() > 0;
+	}
+
+	public List<CatalogArticleAttribute> getAttributeList() {
+		return attributeList;
+	}
+
+	public boolean hasArticles() {
+		return this.articleList != null && this.articleList.size() > 0;
+	}
+
+	public boolean hasPages() {
+		return this.pageList != null && this.pageList.size() > 0;
+	}
+
+	public List<CatalogArticle> getArticleList() {
+		return articleList;
+	};
+	
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("[page name='").append(getName());
+		sb.append("' label='").append(getLabel());
+		sb.append("' renderer='").append(getRenderer());
+		sb.append("']\n");
+		if (hasPages()) {
+			for (ShopPage page : pageList) {
+				sb.append(page.toString()).append("\n");
+			}
+		}
+		if (hasArticles()) {
+			for (CatalogArticle article : articleList) {
+				sb.append(article.toString()).append("\n");
+			}
+		}
+		if (hasAttributes()) {
+			for (CatalogArticleAttribute attribute : attributeList) {
+				sb.append(attribute.toString()).append("\n");
+			}
+		}
+		return sb.toString();
+	}
+
+	public List<CatalogArticle> getAllArticles() {
+		List<CatalogArticle> articles = new ArrayList<CatalogArticle>();
+		if (hasArticles()) {
+			articles.addAll(articleList);
+		}
+		if (hasPages()) {
+			for (ShopPage page : pageList) {
+				articles.addAll(page.getArticles());
+			}
+		}
+		return articles;
+	}
+
+	public ShopViewerComponent getShopViewerComponent() {
+		return this;
+	}
+
+	public List<ShopPage> getPageList() {
+		return pageList;
+	}
+
+	public List<CatalogArticle> getArticles() {
+		return articleList;
+	}
+
+}
