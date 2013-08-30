5555744146e5d66552eb5a564291599ab7e95ba7
rhirning <rhirning@92c39532-7ad5-11e0-8644-75d2b71920e0>
2013-08-30 07:05:53 +0000

    [maven-release-plugin]  copy for tag nicki-parent-3.14

---
+++ nicki/tags/nicki-parent/nicki-parent-3.14/nicki-db/src/main/java/org/mgnl/nicki/db/helper/BasicDBHelper.java
@@ -0,0 +1,180 @@
+/**
+ * This file Copyright (c) 2003-2011 Dr. Ralf Hirning
+ * All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the GNU General
+ * Public License and an individual license with Dr. Ralf
+ * Hirning.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or the individual license, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the individual license, this file and the accompanying
+ * materials are made available under the terms of the
+ * individual license.
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package org.mgnl.nicki.db.helper;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.mgnl.nicki.db.handler.IsExistSelectHandler;
+import org.mgnl.nicki.db.handler.MaxIntValueSelectHandler;
+import org.mgnl.nicki.db.handler.SelectHandler;
+import org.mgnl.nicki.db.handler.SequenceValueSelectHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BasicDBHelper {
+	private static final Logger LOG = LoggerFactory.getLogger(BasicDBHelper.class);
+	public final static String DATE_FORMAT = "yyyy-MM-dd";
+	public final static String TIMESTAMP_FORMAT = "yyyy-MM-dd HH:mm:ss Z";
+	public static SimpleDateFormat format = new SimpleDateFormat(DATE_FORMAT);
+	public static SimpleDateFormat timestampFormat = new SimpleDateFormat(TIMESTAMP_FORMAT);
+
+	public static void executeUpdate(DBProfile profile, String statement) throws Exception {
+		Connection conn = null;
+		Statement stmt = null; // Or PreparedStatement if needed
+		try {
+			conn = profile.getConnection();
+			stmt = conn.createStatement();
+			LOG.debug(statement);
+			stmt.executeUpdate(statement);
+			stmt.close();
+			stmt = null;
+			conn.close(); // Return to connection pool
+			conn = null; // Make sure we don't close it twice
+		} finally {
+			// Always make sure result sets and statements are closed,
+			// and the connection is returned to the pool
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					;
+				}
+				stmt = null;
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					;
+				}
+				conn = null;
+			}
+		}
+	}
+	
+	public static void executeDelete(DBProfile profile, String tableName, String whereClause) throws Exception {
+		executeUpdate(profile, "delete from " + tableName + " where " + whereClause);
+	}
+
+
+	public static boolean isExist(DBProfile profile, String tableName, String whereClause) {
+		IsExistSelectHandler handler = new IsExistSelectHandler(tableName, whereClause);
+		try {
+			select(profile, handler);
+		} catch (Exception e) {
+			LOG.error("Error", e);
+		}
+		return handler.isExist();
+	}
+	
+	public static void select(DBProfile profile, SelectHandler handler) throws Exception {
+
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			conn = profile.getConnection();
+			stmt = conn.createStatement();
+			if (handler.isLoggingEnabled()) {
+				LOG.debug(handler.getSearchStatement());
+			}
+			rs = stmt.executeQuery(handler.getSearchStatement());
+			handler.handle(rs);
+			rs.close();
+			rs = null;
+			
+		} finally {
+			// Always make sure result sets and statements are closed,
+			// and the connection is returned to the pool
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					;
+				}
+				rs = null;
+			}
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					;
+				}
+				stmt = null;
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					;
+				}
+				conn = null;
+			}
+		}
+	}
+
+	public static int getUniqueInt(DBProfile profile, String tableName, String column) {
+		if (!isExist(profile, tableName, "1=1")) {
+			return 1;
+		}
+		MaxIntValueSelectHandler handler = new MaxIntValueSelectHandler(tableName, column);
+		try {
+			select(profile, handler);
+		} catch (Exception e) {
+			LOG.error("Error", e);
+		}
+		return handler.getResult();
+	}
+	
+	public static int getOrderSequenceNumber(DBProfile profile, String sequence) throws Exception {
+		SequenceValueSelectHandler handler = new SequenceValueSelectHandler(sequence);
+		select(profile, handler);
+		return handler.getResult();
+	}
+
+	public static String toString(Date date) {
+		return format.format(date);
+		
+	}
+
+	public static String toDate(Date date) {
+		return "to_date('" + format.format(date) + "','" + DATE_FORMAT + "')";
+		
+	}
+
+}
---
+++ nicki/tags/nicki-parent/nicki-parent-3.14/nicki-shop/src/main/java/org/mgnl/nicki/shop/renderer/BaseShopRenderer.java
@@ -0,0 +1,168 @@
+/**
+ * This file Copyright (c) 2003-2011 Dr. Ralf Hirning
+ * All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the GNU General
+ * Public License and an individual license with Dr. Ralf
+ * Hirning.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or the individual license, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the individual license, this file and the accompanying
+ * materials are made available under the terms of the
+ * individual license.
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package org.mgnl.nicki.shop.renderer;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import org.mgnl.nicki.core.i18n.I18n;
+import org.mgnl.nicki.shop.attributes.AttributeComponent;
+import org.mgnl.nicki.shop.attributes.AttributeComponentFactory;
+import org.mgnl.nicki.shop.attributes.LabelComponent;
+import org.mgnl.nicki.shop.base.objects.CatalogArticle;
+import org.mgnl.nicki.shop.base.objects.CatalogArticleAttribute;
+import org.mgnl.nicki.shop.core.ShopViewerComponent;
+import org.mgnl.nicki.shop.inventory.EndInputListener;
+import org.mgnl.nicki.shop.base.inventory.Inventory;
+import org.mgnl.nicki.shop.base.inventory.InventoryArticle;
+import org.mgnl.nicki.shop.inventory.StartInputListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.vaadin.data.Property.ValueChangeListener;
+import com.vaadin.ui.AbsoluteLayout;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.Layout;
+import com.vaadin.ui.TextArea;
+
+public class BaseShopRenderer {
+	private static final Logger LOG = LoggerFactory.getLogger(BaseShopRenderer.class);
+	
+	private Inventory inventory;
+	
+	protected Component getAttributeComponent(CatalogArticle article, InventoryArticle inventoryArticle, CatalogArticleAttribute articleAttribute, boolean enabled) {
+		try {
+			AttributeComponent<?> attributeComponent = AttributeComponentFactory.getAttributeComponent(articleAttribute.getType());
+			attributeComponent.setEnabled(enabled);
+			return attributeComponent.getInstance(inventory.getUser(), inventory.getPerson(),
+					inventoryArticle, articleAttribute);
+		} catch (Exception e) {
+			return new LabelComponent().getInstance(inventory.getUser(), inventory.getPerson(),
+					inventoryArticle, articleAttribute);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	protected Component getAttributeComponent(CatalogArticle article, CatalogArticleAttribute articleAttribute, boolean enabled, Object value) {
+		try {
+			AttributeComponent<?> attributeComponent = AttributeComponentFactory.getAttributeComponent(articleAttribute.getType());
+			try {
+				((AttributeComponent<Date>)attributeComponent).setValue((Date)value);
+			} catch (Exception e) {
+				((AttributeComponent<String>)attributeComponent).setValue((String)value);
+			}
+			inventory.getInventoryArticle(article).setValue(articleAttribute, value);
+			attributeComponent.setEnabled(enabled);
+			return attributeComponent.getInstance(getInventory().getUser(), getInventory().getPerson(),
+					getInventory().getInventoryArticle(article), articleAttribute);
+		} catch (Exception e) {
+			LOG.error("Error", e);
+			return new LabelComponent().getInstance(getInventory().getUser(), getInventory().getPerson(),
+					getInventory().getInventoryArticle(article), articleAttribute);
+		}
+	}
+
+	protected Component getStartDateComponent(InventoryArticle inventoryArticle, boolean enabled, Date start) {
+		try {
+			AttributeComponent<Date> attributeComponent = AttributeComponentFactory.getAttributeComponent("DATE");
+			attributeComponent.setValue(start);
+			inventoryArticle.setStart(start);
+			attributeComponent.setEnabled(enabled);
+			ValueChangeListener listener = new StartInputListener(inventoryArticle);
+			return attributeComponent.getInstance(I18n.getText(CatalogArticle.CAPTION_START), start, listener);
+		} catch (Exception e) {
+			LOG.error("Error", e);
+			// TODO: convert date
+			return new LabelComponent().getInstance(I18n.getText(CatalogArticle.CAPTION_START), start.toString(), null);
+		}
+	}
+
+	protected Component getEndDateComponent(InventoryArticle inventoryArticle, boolean enabled, Date end) {
+		try {
+			AttributeComponent<Date> attributeComponent = AttributeComponentFactory.getAttributeComponent("DATE");
+			attributeComponent.setValue(end);
+			inventoryArticle.setEnd(end);
+			attributeComponent.setEnabled(enabled);
+			ValueChangeListener listener = new EndInputListener(inventoryArticle);
+			return attributeComponent.getInstance(I18n.getText(CatalogArticle.CAPTION_END), end, listener);
+		} catch (Exception e) {
+			LOG.error("Error", e);
+			// TODO: convert date
+			return new LabelComponent().getInstance(I18n.getText(CatalogArticle.CAPTION_END), end.toString(), null);
+		}
+	}
+
+	protected void removeExcept(Layout parent, Component button) {
+		List<Component> toBeRemoved = new ArrayList<Component>();
+		for (Iterator<Component> iterator = parent.iterator(); iterator.hasNext();) {
+			Component component = iterator.next();
+			if (component != button) {
+				toBeRemoved.add(component);
+			}
+		}
+		for (Component component : toBeRemoved) {
+			parent.removeComponent(component);
+		}
+	}
+	
+
+	public Component getXMLComponent(ShopViewerComponent shopViewerComponent) {
+		AbsoluteLayout layout = new AbsoluteLayout();
+		layout.setHeight("420px");
+		// textArea_1
+		TextArea xml = new TextArea();
+		xml.setWidth("100%");
+		xml.setHeight("100%");
+		xml.setImmediate(false);
+		xml.setValue(shopViewerComponent.toString());
+		layout.addComponent(xml, "top:20.0px;left:20.0px;right:20.0px;");
+
+		return layout;
+	}
+	
+	public void setInventory(Inventory inventory) {
+		this.inventory = inventory;
+	}
+
+	public Inventory getInventory() {
+		return inventory;
+	}
+
+
+
+
+
+
+}
