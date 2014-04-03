21a32d59b9cb89fdafa536ad900fe47bbda35f01
rhirning <rhirning@92c39532-7ad5-11e0-8644-75d2b71920e0>
2014-04-03 09:16:17 +0000

    [maven-release-plugin]  copy for tag nicki-parent-3.31

---
+++ nicki/tags/nicki-parent/nicki-parent-3.31/nicki-vaadin-base/src/main/java/org/mgnl/nicki/vaadin/base/data/ListAttributeDataContainer.java
@@ -0,0 +1,81 @@
+/**
+ * This file Copyright (c) 2003-2011 Dr. Ralf Hirning
+ * All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the GNU General
+ * Public License and an individual license with Dr. Ralf
+ * Hirning.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or the individual license, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the individual license, this file and the accompanying
+ * materials are made available under the terms of the
+ * individual license.
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package org.mgnl.nicki.vaadin.base.data;
+
+import org.mgnl.nicki.core.objects.DynamicObject;
+
+import com.vaadin.data.Property;
+
+@SuppressWarnings("serial")
+public class ListAttributeDataContainer<T> implements DataContainer<T>, Property<T> {
+
+	public DynamicObject getDynamicObject() {
+		return dynamicObject;
+	}
+
+	public String getAttributeName() {
+		return attributeName;
+	}
+
+	private DynamicObject dynamicObject;
+	private String attributeName;
+	private boolean readOnly = false;
+	
+	public ListAttributeDataContainer(DynamicObject dynamicObject, String attributeName) {
+		this.dynamicObject = dynamicObject;
+		this.attributeName = attributeName;
+	}
+
+	@SuppressWarnings("unchecked")
+	public T getValue() {
+		return (T) dynamicObject.get(attributeName);
+	}
+
+	public void setValue(Object newValue) throws ReadOnlyException {
+		dynamicObject.put(attributeName, newValue);
+	}
+
+	@SuppressWarnings("unchecked")
+	public Class<? extends T> getType() {
+		return (Class<? extends T>) dynamicObject.getModel().getDynamicAttribute(attributeName).getClass();
+	}
+
+	public boolean isReadOnly() {
+		return readOnly;
+	}
+
+	public void setReadOnly(boolean newStatus) {
+		this.readOnly = newStatus;
+	}
+
+}
