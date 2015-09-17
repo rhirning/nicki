3ae5a32dc81b7d69a0e2f5ae45d566713700df07
rhirning <rhirning@92c39532-7ad5-11e0-8644-75d2b71920e0>
2015-09-17 16:00:12 +0000

    [maven-release-plugin]  copy for tag nicki-parent-4.25

---
+++ nicki/tags/nicki-parent/nicki-parent-4.25/nicki-user-admin/src/main/java/org/mgnl/nicki/user/admin/app/UserAdmin.java
@@ -0,0 +1,78 @@
+/**
+ * This file Copyright (c) 2003-2015 Dr. Ralf Hirning
+ * All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the GNU General
+ * Public License and an individual license with Dr. Ralf
+ * Hirning.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or the individual license, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the individual license, this file and the accompanying
+ * materials are made available under the terms of the
+ * individual license.
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package org.mgnl.nicki.user.admin.app;
+
+
+import org.mgnl.nicki.app.menu.application.MainView;
+import org.mgnl.nicki.core.config.Config;
+import org.mgnl.nicki.core.context.Target;
+import org.mgnl.nicki.core.context.TargetFactory;
+import org.mgnl.nicki.core.helper.DataHelper;
+import org.mgnl.nicki.core.i18n.I18n;
+import org.mgnl.nicki.dynamic.objects.objects.Person;
+import org.mgnl.nicki.vaadin.base.application.AccessGroup;
+import org.mgnl.nicki.vaadin.base.application.NickiApplication;
+import com.vaadin.ui.Component;
+
+@AccessGroup(name = {"nickiAdmins", "IDM-Development"})
+@SuppressWarnings("serial")
+public class UserAdmin extends NickiApplication {
+	
+	public UserAdmin() {
+		super();
+		setUseWelcomeDialog(DataHelper.booleanOf(Config.getProperty("nicki.projects.useWelcomeDialog", "false")));
+	}
+
+	@Override
+	public Component getEditor() {
+		MainView mainView = new MainView((Person) getNickiContext().getUser());
+		
+		mainView.addNavigationEntry(I18n.getText("Administration"),
+				I18n.getText("User"), new UserView());
+
+		mainView.initNavigation();
+		
+		return mainView;
+	}
+
+	@Override
+	public Target getTarget() {
+		return TargetFactory.getDefaultTarget();
+	}
+
+	@Override
+	public String getI18nBase() {
+		return "nicki.editor.script";
+	}
+	
+}
