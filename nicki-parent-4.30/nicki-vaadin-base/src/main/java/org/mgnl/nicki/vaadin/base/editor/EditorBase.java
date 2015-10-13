4d0d2c5b8be541bdb04cba6cd4cf9a9d20939d48
rhirning <31392706+rhirning@users.noreply.github.com>
2015-10-13 07:12:00 +0000

    [maven-release-plugin]  copy for tag nicki-parent-4.30

---
+++ nicki-parent-4.30/nicki-vaadin-base/src/main/java/org/mgnl/nicki/vaadin/base/editor/EditorBase.java
@@ -0,0 +1,60 @@
+/**
+ * This file Copyright (c) 2003-2011 Dr. Ralf Hirning
+ * All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the GNU General
+ * Public License and an individual license with Dr. Ralf
+ * Hirning.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or the individual license, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the individual license, this file and the accompanying
+ * materials are made available under the terms of the
+ * individual license.
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package org.mgnl.nicki.vaadin.base.editor;
+
+import java.io.Serializable;
+
+import org.mgnl.nicki.core.context.NickiContext;
+
+
+import com.vaadin.ui.CustomComponent;
+
+@SuppressWarnings("serial")
+public abstract class EditorBase extends CustomComponent implements Serializable {
+	public EditorBase(NickiContext context) {
+		super();
+		this.context = context;
+	}
+
+	private NickiContext context;
+
+	public void setContext(NickiContext context) {
+		this.context = context;
+	}
+
+	public NickiContext getContext() {
+		return context;
+	}
+
+
+}
