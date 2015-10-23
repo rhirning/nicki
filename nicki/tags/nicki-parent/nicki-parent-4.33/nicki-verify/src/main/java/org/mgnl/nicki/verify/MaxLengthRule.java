c2d46f68bdb5544fc64adae687c6ecb09a9fb2f9
rhirning <rhirning@92c39532-7ad5-11e0-8644-75d2b71920e0>
2015-10-23 12:28:26 +0000

    [maven-release-plugin]  copy for tag nicki-parent-4.33

---
+++ nicki/tags/nicki-parent/nicki-parent-4.33/nicki-verify/src/main/java/org/mgnl/nicki/verify/MaxLengthRule.java
@@ -0,0 +1,30 @@
+package org.mgnl.nicki.verify;
+
+import org.apache.commons.lang.StringUtils;
+import org.mgnl.nicki.core.i18n.I18n;
+
+public class MaxLengthRule extends Rule {
+	private static final long serialVersionUID = 1L;
+	private int length = 0;
+
+	public MaxLengthRule(String value) {
+		setParameter(value);
+		this.length = Integer.parseInt(value);
+	}
+
+	@Override
+	public boolean evaluate(String value) {
+		return StringUtils.length(value) <= this.length;
+	}
+	
+	@Override
+	public String getMessage() {
+		return I18n.getText(getI18nBase() + ".maxlength", getParameter());
+	}
+	public String toString() {
+		return "maxLength:" + length;
+	}
+
+
+
+}
