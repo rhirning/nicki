63ecdfdc1cf86f4d1bafba357df2b872dee2d0d1
rhirning <31392706+rhirning@users.noreply.github.com>
2015-11-06 13:43:21 +0000

    [maven-release-plugin]  copy for tag nicki-parent-5.02

---
+++ nicki-parent-5.02/nicki-db/src/main/java/org/mgnl/nicki/db/profile/DBProfileManager.java
@@ -0,0 +1,61 @@
+package org.mgnl.nicki.db.profile;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.mgnl.nicki.core.config.Config;
+import org.mgnl.nicki.core.helper.DataHelper;
+
+
+public class DBProfileManager {
+
+	private static final String PROPERTY_PROFILES = "nicki.db.profiles";
+	private static final String PROPERTY_PROFILES_BASE = "nicki.db.profile";
+	private static final String PROPERTY_PROFILES_DATA_SOURCE = "datasource";
+	private static final String PROPERTY_PROFILES_CONNECTION_TYPE = "type";
+	private static final String SEPARATOR = ",";
+	
+	
+	private static DBProfileManager instance = null;
+	private Map<String, DBProfile> profiles = new HashMap<String, DBProfile>();
+
+	public static DBProfileManager getInstance() {
+		if (instance == null) {
+			instance = new DBProfileManager();
+			instance.init();
+		}
+		return instance;
+	}
+	
+	public void init() {
+		profiles.clear();
+		String profileConfigurations = Config.getProperty(PROPERTY_PROFILES);
+		if (StringUtils.isNotEmpty(profileConfigurations)) {
+			for (String profile : DataHelper.getList(profileConfigurations, SEPARATOR)) {
+				addProfile(profile);
+				
+			}
+		}
+	}
+
+	private void addProfile(String name) {
+		String profileBase = PROPERTY_PROFILES_BASE + "." + name + ".";
+		String dataSource = Config.getProperty(profileBase + PROPERTY_PROFILES_DATA_SOURCE);
+		String type  = Config.getProperty(profileBase + PROPERTY_PROFILES_CONNECTION_TYPE);
+		
+		if (StringUtils.isNotBlank(dataSource)) {
+			if (StringUtils.equals("dbcp", type)) {
+				profiles.put(name, new DBCPProfile(dataSource));
+			} else {
+				profiles.put(name, new JndiDBProfile(dataSource));
+			}
+		}
+	}
+	
+	public static DBProfile getProfile(String name) {
+		return getInstance().profiles.get(name);
+	}
+	
+	
+}
