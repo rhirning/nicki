164b039d2cf287c4e8c3321326ed9ca21a4ba55c
rhirning <31392706+rhirning@users.noreply.github.com>
2015-12-08 07:38:28 +0000

    [maven-release-plugin]  copy for tag nicki-parent-5.12

---
+++ nicki-parent-5.12/nicki-db/src/main/java/org/mgnl/nicki/db/context/mysql/MySqlContext.java
@@ -0,0 +1,13 @@
+package org.mgnl.nicki.db.context.mysql;
+
+import org.mgnl.nicki.db.context.BaseDBContext;
+import org.mgnl.nicki.db.context.DBContext;
+import org.mgnl.nicki.db.profile.DBProfile;
+
+public class MySqlContext extends BaseDBContext implements DBContext {
+
+	public MySqlContext(DBProfile profile) {
+	}
+
+
+}
