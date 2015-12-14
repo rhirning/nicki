003ae217a232826c27221e3322d0f3e07953d62e
rhirning <31392706+rhirning@users.noreply.github.com>
2015-12-14 09:07:59 +0000

    [maven-release-plugin]  copy for tag nicki-parent-5.15

---
+++ nicki-parent-5.15/nicki-db/src/main/java/org/mgnl/nicki/db/context/BaseDBContext.java
@@ -0,0 +1,473 @@
+package org.mgnl.nicki.db.context;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.mgnl.nicki.db.annotation.Attribute;
+import org.mgnl.nicki.db.annotation.SubTable;
+import org.mgnl.nicki.db.annotation.Table;
+import org.mgnl.nicki.db.handler.ListSelectHandler;
+import org.mgnl.nicki.db.handler.SelectHandler;
+import org.mgnl.nicki.db.profile.DBProfile;
+import org.mgnl.nicki.db.profile.InitProfileException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BaseDBContext implements DBContext {
+	public final static String TIMESTAMP_ORACLE = "YYYY-MM-DD HH24:MI:SS";
+	public final static String TIMESTAMP_FOR_ORACLE = "yyyy-MM-dd HH:mm:ss";
+	public static SimpleDateFormat timestampOracle = new SimpleDateFormat(TIMESTAMP_FOR_ORACLE);
+	private static final Logger LOG = LoggerFactory.getLogger(BaseDBContext.class);
+	private DBProfile profile;
+	private Connection connection;
+	private String schema;
+	
+	public BaseDBContext() {
+	}
+
+	@Override
+	public void setProfile(DBProfile profile) {
+		this.profile = profile;
+	}
+
+	@Override
+	public <T> T create(T bean) throws SQLException, InitProfileException, NotSupportedException {
+		boolean inTransaction = false;
+		if (this.connection != null) {
+			inTransaction = true;
+		} else {
+			beginTransaction();
+		}
+		
+		
+		try {
+			Long primaryKey = _create(bean);
+			if (hasSubs(bean)) {
+				for (Object sub : getSubs(bean, primaryKey)) {
+					create(sub);
+				}
+			}
+			
+			if (!inTransaction) {
+				try {
+					commit();
+				} catch (NotInTransactionException e) {
+					;
+				}
+			}
+			return load(bean);
+		} finally {
+			if (!inTransaction) {
+				try {
+					rollback();
+				} catch (NotInTransactionException e) {
+					;
+				}
+			}
+		}
+	}
+	
+	private Collection<Object> getSubs(Object bean, long primaryKey) {
+		Collection<Object> list = new ArrayList<>();
+		for (Field field : bean.getClass().getDeclaredFields()) {
+			try {
+				if (field.getAnnotation(SubTable.class) != null) {
+						if (Collection.class.isAssignableFrom(field.getType())) {
+							String getter = "get" + StringUtils.capitalize(field.getName());
+							Method method;
+							method = bean.getClass().getMethod(getter);
+							@SuppressWarnings("unchecked")
+							Collection<Object> fieldList = (Collection<Object>) method.invoke(bean);
+							if (fieldList != null && fieldList.size() > 0) {
+								list.addAll(fieldList);
+							}
+						} else {
+							String getter = "get" + StringUtils.capitalize(field.getName());
+							Method method = bean.getClass().getMethod(getter);
+							Object fieldEntry = method.invoke(bean);
+							if (fieldEntry != null) {
+								list.add(fieldEntry);
+							}
+						}
+				}
+				for (Object object : list) {
+					setPrimaryKey(object, primaryKey);
+				}
+			} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
+
+	private void setPrimaryKey(Object bean, long primaryKey) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		for (Field field : bean.getClass().getDeclaredFields()) {
+			if (field.getAnnotation(Attribute.class) != null) {
+				Attribute attribute = field.getAnnotation(Attribute.class);
+				if (attribute.foreignKey()) {
+					String setter = "set" + StringUtils.capitalize(field.getName());
+					Method method = bean.getClass().getMethod(setter, long.class);
+					method.invoke(bean, primaryKey);
+				}
+			}
+		}
+	}
+
+	private boolean hasSubs(Object bean) {
+		for (Field field : bean.getClass().getDeclaredFields()) {
+			if (field.getAnnotation(SubTable.class) != null) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private <T> Long _create(T bean) throws SQLException, NotSupportedException {
+		try(Statement stmt = this.connection.createStatement()) {
+			String statement = createInsertStatement(bean);
+			LOG.debug(statement);
+			stmt.executeUpdate(statement, Statement.RETURN_GENERATED_KEYS);
+			ResultSet generatedKeys = stmt.getGeneratedKeys();
+			if (generatedKeys != null && generatedKeys.first()) {
+				return new Long(generatedKeys.getInt(1));
+			} else {
+				return null;
+			}
+		}
+		
+	}
+
+	@Override
+	public <T> T update(T bean) throws SQLException, InitProfileException {
+		boolean inTransaction = false;
+		if (this.connection != null) {
+			inTransaction = true;
+		} else {
+			beginTransaction();
+		}
+		
+		try {
+			try(Statement stmt = this.connection.createStatement()) {
+				String statement = createUpdateStatement(bean);
+				LOG.debug(statement);
+				stmt.executeUpdate(statement);
+				if (!inTransaction) {
+					try {
+						commit();
+					} catch (NotInTransactionException e) {
+						;
+					}
+				}
+				return load(bean);
+			}
+		} finally {
+			if (!inTransaction) {
+				try {
+					rollback();
+				} catch (NotInTransactionException e) {
+					;
+				}
+			}
+		}
+	}
+
+	@Override
+	public <T> void delete(T bean) throws SQLException, InitProfileException {
+		boolean inTransaction = false;
+		if (this.connection != null) {
+			inTransaction = true;
+		} else {
+			beginTransaction();
+		}
+		
+		try {
+			try(Statement stmt = this.connection.createStatement()) {
+				String statement = createDeleteStatement(bean);
+				LOG.debug(statement);
+				stmt.executeUpdate(statement);
+				if (!inTransaction) {
+					try {
+						commit();
+					} catch (NotInTransactionException e) {
+						;
+					}
+				}
+			}
+		} finally {
+			if (!inTransaction) {
+				try {
+					rollback();
+				} catch (NotInTransactionException e) {
+					;
+				}
+			}
+		}
+	}
+
+	@Override
+	public <T> List<T> select(Class<T> clazz, ListSelectHandler<T> handler) throws SQLException, InitProfileException {
+		boolean inTransaction = false;
+		if (this.connection != null) {
+			inTransaction = true;
+		} else {
+			beginTransaction();
+		}
+
+		try {
+			try(Statement stmt = this.connection.createStatement()) {
+				if (handler.isLoggingEnabled()) {
+					LOG.debug(handler.getSearchStatement());
+				}
+				try(ResultSet rs = stmt.executeQuery(handler.getSearchStatement())) {
+					handler.handle(rs);
+					return handler.getResults();
+				}
+			}
+		} finally {
+			if (!inTransaction) {
+				try {
+					rollback();
+				} catch (NotInTransactionException e) {
+					;
+				}
+			}
+		}
+	}
+
+	@Override
+	public void select(SelectHandler handler) throws SQLException, InitProfileException {
+		boolean inTransaction = false;
+		if (this.connection != null) {
+			inTransaction = true;
+		} else {
+			beginTransaction();
+		}
+
+		try {
+			try(Statement stmt = this.connection.createStatement()) {
+				if (handler.isLoggingEnabled()) {
+					LOG.debug(handler.getSearchStatement());
+				}
+				try(ResultSet rs = stmt.executeQuery(handler.getSearchStatement())) {
+					handler.handle(rs);
+				}
+			}
+		} finally {
+			if (!inTransaction) {
+				try {
+					commit();
+				} catch (NotInTransactionException e) {
+					;
+				}
+			}
+		}
+	}
+
+	@Override
+	public Connection beginTransaction() throws SQLException, InitProfileException {
+		if (this.connection == null) {
+			this.connection = profile.getConnection();
+		}
+		return this.connection;
+	}
+
+	@Override
+	public void commit() throws NotInTransactionException, SQLException {
+		if (this.connection == null) {
+			throw new NotInTransactionException();
+		}
+		try {
+			this.connection.commit();
+		} finally {
+			// Always make sure result sets and statements are closed,
+			// and the connection is returned to the pool
+			if (this.connection != null) {
+				try {
+					this.connection.close();
+				} catch (SQLException e) {
+					;
+				}
+				this.connection = null;
+			}
+		}
+	}
+
+	@Override
+	public void rollback() throws NotInTransactionException, SQLException {
+		if (this.connection == null) {
+			throw new NotInTransactionException();
+		}
+		try {
+			this.connection.rollback();
+		} finally {
+			// Always make sure result sets and statements are closed,
+			// and the connection is returned to the pool
+			if (this.connection != null) {
+				try {
+					this.connection.close();
+				} catch (SQLException e) {
+					;
+				}
+				this.connection = null;
+			}
+		}
+	}
+
+	private<T> T load(T bean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> String createInsertStatement(T bean) throws NotSupportedException {
+		/**
+		 * insert into SCHEMA.TABLE (a,b,c) values (" ", " ", 2);
+		 */
+		
+		Table table = bean.getClass().getAnnotation(Table.class);
+		if (table == null) {
+			throw new NotSupportedException();
+		}
+		
+		Map<String, String> columnValues = new HashMap<>();
+		
+		for (Field field : bean.getClass().getDeclaredFields()) {
+			if (field.getAnnotation(Attribute.class)!= null) {
+				Attribute attribute = field.getAnnotation(Attribute.class);
+				try {
+					if (field.getType() == String.class) {
+						columnValues.put(attribute.name(), getStringValue(bean, field));
+					} else if (field.getType() == Date.class) {
+						columnValues.put(attribute.name(), getDateValue(bean, field, attribute));
+					} else if (field.getType() == long.class) {
+						columnValues.put(attribute.name(), getLongValue(bean, field, attribute));
+					} else if (field.getType() == int.class) {
+						columnValues.put(attribute.name(), getIntValue(bean, field, attribute));
+					}
+				} catch (NoSuchMethodException | SecurityException | IllegalAccessException
+						| IllegalArgumentException | InvocationTargetException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		// TODO Auto-generated method stub
+		return getInsertStatement(getQualifiedTableName(bean.getClass()), columnValues);
+	}
+
+	protected String getLongValue(Object bean, Field field, Attribute attribute) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		return ((Long) getValue(bean, field)).toString();
+	}
+
+	protected String getIntValue(Object bean, Field field, Attribute attribute) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		return ((Integer) getValue(bean, field)).toString();
+	}
+
+	public String getQualifiedTableName(Class<? extends Object> clazz) throws NotSupportedException {
+		
+		Table table = clazz.getAnnotation(Table.class);
+		if (table == null) {
+			throw new NotSupportedException();
+		}
+		if (schema != null) {
+			return schema + "." + table.name();
+		} else {
+			return table.name();
+		}
+	}
+
+	@Override
+	public Object getColumn(Class<? extends Object> clazz, String fieldName) throws NoSuchFieldException {
+
+		try {
+			Field field = clazz.getDeclaredField(fieldName);
+			if (field.getAnnotation(Attribute.class)!= null) {
+				Attribute attribute = field.getAnnotation(Attribute.class);
+				return attribute.name();
+			} else {
+				return null;
+			}
+		} catch (NoSuchFieldException | SecurityException e) {
+			throw new NoSuchFieldException(fieldName);
+		}
+				
+	}
+
+	protected static String getInsertStatement(String tableName, Map<String, String> columnValues) {
+		ColumnsAndValues cv = new ColumnsAndValues("","");
+		for (String columnName : columnValues.keySet()) {
+			cv.add(columnName, columnValues.get(columnName));
+		}
+
+		return "insert into " + tableName + " (" + cv.getColumns() + ") values (" + cv.getValues() + ")";
+	}
+
+	protected String getDateValue(Object bean, Field field, Attribute attribute) {
+		try {
+			Date date;
+			if (attribute.now()) {
+				date = new Date();
+			} else {
+				date = (Date) getValue(bean, field);
+			}
+			return toTimestamp(date);
+		} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	protected String toTimestamp(Date date) {
+		return "to_date('" + timestampOracle.format(date) + "','" + TIMESTAMP_ORACLE + "')";
+	}
+
+	protected String getStringValue(Object bean, Field field) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		return "'" + (String) getValue(bean, field) + "'";
+	}
+
+	protected Object getValue(Object bean, Field field) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		String getter = "get" + StringUtils.capitalize(field.getName());
+		Method method = bean.getClass().getMethod(getter);
+		return method.invoke(bean);
+	}
+
+	@Override
+	public <T> String createUpdateStatement(T bean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> String createDeleteStatement(T bean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getSchema() {
+		return schema;
+	}
+
+	public void setSchema(String schema) {
+		this.schema = schema;
+	}
+
+}
