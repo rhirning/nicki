2ff1a5028ec81f11bc33b927be46cfc2cf09a310
rhirning <31392706+rhirning@users.noreply.github.com>
2015-12-14 07:03:11 +0000

    Nicki DB erweitert

--- nicki-db/src/main/java/org/mgnl/nicki/db/context/DBContextManager.java
+++ nicki-db/src/main/java/org/mgnl/nicki/db/context/DBContextManager.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.apache.commons.lang.StringUtils;
 import org.mgnl.nicki.core.config.Config;
 import org.mgnl.nicki.core.helper.DataHelper;
+import org.mgnl.nicki.db.connection.InvalidConfigurationException;
 import org.mgnl.nicki.db.profile.DBCPProfile;
 import org.mgnl.nicki.db.profile.DBProfile;
 import org.mgnl.nicki.db.profile.JndiDBProfile;
@@ -16,12 +17,13 @@ import org.slf4j.LoggerFactory;
 public class DBContextManager {
 	private static final Logger LOG = LoggerFactory.getLogger(DBContextManager.class);
 
-	private static final String PROPERTY_PROFILES = "nicki.db.profiles";
-	private static final String PROPERTY_PROFILES_BASE = "nicki.db.profile";
-	private static final String PROPERTY_PROFILES_DATA_SOURCE = "datasource";
-	private static final String PROPERTY_PROFILES_CONNECTION_TYPE = "type";
-	private static final String PROPERTY_PROFILES_CONTEXT_CLASS_NAME = "contextClassName";
-	private static final String PROPERTY_PROFILES_AUTO_COMMIT = "autocommit";
+	private static final String PROPERTY_CONTEXTS = "nicki.db.contexts";
+	private static final String PROPERTY_CONTEXT_BASE = "nicki.db.context";
+	private static final String PROPERTY_CONTEXT_CLASS_NAME = "contextClassName";
+	private static final String PROPERTY_CONTEXT_DATA_SOURCE = "datasource";
+	private static final String PROPERTY_CONTEXT_CONNECTION_TYPE = "type";
+	private static final String PROPERTY_CONTEXT_SCHEMA = "schema";
+	private static final String PROPERTY_CONTEXT_AUTO_COMMIT = "autocommit";
 	private static final String SEPARATOR = ",";
 	
 	
@@ -38,18 +40,24 @@ public class DBContextManager {
 	
 	public void init() {
 		contexts.clear();
-		String profileConfigurations = Config.getProperty(PROPERTY_PROFILES);
-		if (StringUtils.isNotEmpty(profileConfigurations)) {
-			for (String profileName : DataHelper.getList(profileConfigurations, SEPARATOR)) {
+		String contextsConfigurations = Config.getProperty(PROPERTY_CONTEXTS);
+		if (StringUtils.isNotEmpty(contextsConfigurations)) {
+			for (String contextName : DataHelper.getList(contextsConfigurations, SEPARATOR)) {
 				try {
-					DBProfile profile = createProfile(profileName);
+					String contextBase = PROPERTY_CONTEXT_BASE + "." + contextName + ".";
+					String contextClassName = Config.getProperty(contextBase
+							+ PROPERTY_CONTEXT_CLASS_NAME);
+					DBContext context = (DBContext) getClass().getClassLoader().loadClass(contextClassName).newInstance();
+					
+					String schema = Config.getProperty(contextBase + PROPERTY_CONTEXT_SCHEMA);
+					if (StringUtils.isNotBlank(schema)) {
+						context.setSchema(schema);
+					}
+					
+					DBProfile profile = createProfile(contextName);
 					if (profile != null) {
-						String contextClassName = Config.getProperty(PROPERTY_PROFILES_BASE
-								+ "." + profileName
-								+ "." +PROPERTY_PROFILES_CONTEXT_CLASS_NAME);
-						DBContext context = (DBContext) getClass().getClassLoader().loadClass(contextClassName).newInstance();
 						context.setProfile(profile);
-						contexts.put(profileName, context);
+						contexts.put(contextName, context);
 					}
 				} catch (Exception e) {
 					LOG.error("error init DBContexts", e);
@@ -58,20 +66,18 @@ public class DBContextManager {
 		}
 	}
 
-	private DBProfile createProfile(String name) {
-		String profileBase = PROPERTY_PROFILES_BASE + "." + name + ".";
-		String dataSource = Config.getProperty(profileBase + PROPERTY_PROFILES_DATA_SOURCE);
-		String type  = Config.getProperty(profileBase + PROPERTY_PROFILES_CONNECTION_TYPE);
-		boolean autoCommit  = DataHelper.booleanOf(Config.getProperty(profileBase + PROPERTY_PROFILES_AUTO_COMMIT, "false")) ;
+	private DBProfile createProfile(String contextName) throws InvalidConfigurationException {
+		String contextBase = PROPERTY_CONTEXT_BASE + "." + contextName + ".";
+		String type  = Config.getProperty(contextBase + PROPERTY_CONTEXT_CONNECTION_TYPE);
+		boolean autoCommit  = DataHelper.booleanOf(Config.getProperty(contextBase + PROPERTY_CONTEXT_AUTO_COMMIT, "false")) ;
 		
-		if (StringUtils.isNotBlank(dataSource)) {
-			if (StringUtils.equals("dbcp", type)) {
-				return new DBCPProfile(dataSource, autoCommit);
-			} else {
-				return new JndiDBProfile(dataSource, autoCommit);
-			}
+		if (StringUtils.equals("dbcp", type)) {
+			String profileConfigBase = contextBase + "dbcp.";
+			return new DBCPProfile(profileConfigBase, autoCommit);
+		} else {
+			String dataSource = Config.getProperty(contextBase + PROPERTY_CONTEXT_DATA_SOURCE);
+			return new JndiDBProfile(dataSource, autoCommit);
 		}
-		return null;
 	}
 	
 	public static DBContext getContext(String name) {
--- nicki-db/src/main/java/org/mgnl/nicki/db/profile/DBCPProfile.java
+++ nicki-db/src/main/java/org/mgnl/nicki/db/profile/DBCPProfile.java
@@ -4,20 +4,22 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 import org.mgnl.nicki.db.connection.ConnectionManager;
+import org.mgnl.nicki.db.connection.InvalidConfigurationException;
 
 
 public class DBCPProfile implements DBProfile {
-	private String name = null;
 	private boolean autoCommit = false;
+	private ConnectionManager connectionManager;
 
-	public DBCPProfile(String name, boolean autoCommit) {
+	public DBCPProfile(String profileConfigBase, boolean autoCommit) throws InvalidConfigurationException {
 		super();
-		this.name = name;
 		this.autoCommit = autoCommit;
+		this.connectionManager = new ConnectionManager();
+		this.connectionManager.init(profileConfigBase);
 	}
 
 	public Connection getConnection() throws SQLException  {
-		Connection connection = ConnectionManager.getInstance().getConnection(this.name);
+		Connection connection = this.connectionManager.getConnection();
 		connection.setAutoCommit(this.autoCommit);
 		return connection;
 	}
