46048fc33555e2a297d7620ea4fd56ff2bf03deb
rhirning <31392706+rhirning@users.noreply.github.com>
2015-12-18 08:10:39 +0000

    [maven-release-plugin]  copy for tag nicki-parent-5.19

---
+++ nicki-parent-5.19/nicki-core/src/main/java/org/mgnl/nicki/core/visitor/Visitor.java
@@ -0,0 +1,6 @@
+package org.mgnl.nicki.core.visitor;
+
+public interface Visitor {
+	public static enum ACTION {FINISH, CONTINUE};
+
+}
---
+++ nicki-parent-5.19/nicki-vaadin-base/src/main/java/org/mgnl/nicki/vaadin/base/dialog/NavigationLabel.java
@@ -0,0 +1,15 @@
+package org.mgnl.nicki.vaadin.base.dialog;
+
+public class NavigationLabel {
+	private String caption;
+
+	public NavigationLabel(String caption) {
+		super();
+		this.caption = caption;
+	}
+
+	public String getCaption() {
+		return caption;
+	}
+
+}
