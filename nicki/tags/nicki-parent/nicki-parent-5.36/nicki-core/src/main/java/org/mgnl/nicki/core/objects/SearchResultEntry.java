79bbde2cb56f2a3d0bebd29f92c45aee25af0608
rhirning <rhirning@92c39532-7ad5-11e0-8644-75d2b71920e0>
2016-02-05 11:27:15 +0000

    [maven-release-plugin] copy for tag nicki-parent-5.36

---
+++ nicki/tags/nicki-parent/nicki-parent-5.36/nicki-core/src/main/java/org/mgnl/nicki/core/objects/SearchResultEntry.java
@@ -0,0 +1,64 @@
+/**
+ * This file Copyright (c) 2003-2011 Dr. Ralf Hirning
+ * All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the GNU General
+ * Public License and an individual license with Dr. Ralf
+ * Hirning.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or the individual license, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the individual license, this file and the accompanying
+ * materials are made available under the terms of the
+ * individual license.
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package org.mgnl.nicki.core.objects;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SearchResultEntry implements Serializable {
+	private static final long serialVersionUID = -6784848780254677616L;
+	private Map<String, Object> values = new HashMap<String, Object>();
+	private String dn;
+	
+	public String getDn() {
+		return dn;
+	}
+
+	public void setDn(String dn) {
+		this.dn = dn;
+	}
+
+	public void addValue(String key, Object value) {
+		this.values.put(key, value);
+	}
+	
+	public Object getValue(String key) {
+		return this.values.get(key);
+	}
+
+	public Map<String, Object> getValues() {
+		return values;
+	}
+	
+}
