a34ec21ae11942550d1339d1d1158965be7995fb
rhirning <31392706+rhirning@users.noreply.github.com>
2016-03-04 14:50:47 +0000

    Zugriff auf Umgebungsvariable

--- nicki-core/src/main/java/org/mgnl/nicki/core/helper/Environment.java
+++ nicki-core/src/main/java/org/mgnl/nicki/core/helper/Environment.java
@@ -1,13 +1,8 @@
 package org.mgnl.nicki.core.helper;
 
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.naming.Context;
 import javax.naming.InitialContext;
-import javax.naming.NamingException;
-
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,29 +11,16 @@ import org.slf4j.LoggerFactory;
 public class Environment implements Serializable{
 	
 	private static final Logger LOG = LoggerFactory.getLogger(Environment.class);
-	private static Environment instance;
-	private Map<String, String> map;
-	
-	public Environment() {
-		this.map = new HashMap<>();
-	}
-
-	public static void setProperty(String name, String value) {
-		getInstance()._put(name, value);
-	}
 	
 	public static String getProperty(String name) {
-		if (getFromSystemEnvironment(name) != null) {
-			return getFromSystemEnvironment(name);
-		}
-		if (getPropertyFromSystemEnvironment(name) != null) {
-			return getPropertyFromSystemEnvironment(name);
-		}
 		if (getFromJndiContext(name) != null) {
 			return getFromJndiContext(name);
 		}
-		if (contains(name)) {
-			return getInstance()._get(name);
+		if (getFromSystemProperty(name) != null) {
+			return getFromSystemProperty(name);
+		}
+		if (getFromSystemEnvironment(name) != null) {
+			return getFromSystemEnvironment(name);
 		}
 		
 		return null;
@@ -59,7 +41,7 @@ public class Environment implements Serializable{
 		return null;
 	}
 
-	public static String getPropertyFromSystemEnvironment(String name) {
+	public static String getFromSystemProperty(String name) {
 		try {
 			return System.getProperty(name);
 		} catch (Exception e) {
@@ -75,32 +57,6 @@ public class Environment implements Serializable{
 			LOG.debug("Error accessing System.getenv: " + name, e);
 		}
 		return null;
-	}
-
-	public static boolean contains(String name) {
-		if (instance == null) {
-			return false;
-		}
-		return getInstance()._contains(name);
-	}
-
-	private boolean _contains(String name) {
-		return this.map.containsKey(name);
-	}
-
-	private String _get(String name) {
-		return this.map.get(name);
-	}
-
-	private void _put(String name, String value) {
-		this.map.put(name, value);
-	}
-
-	private synchronized static Environment getInstance() {
-		if (instance == null) {
-			instance = new Environment();
-		}
-		return instance;
 	}
 
 }
