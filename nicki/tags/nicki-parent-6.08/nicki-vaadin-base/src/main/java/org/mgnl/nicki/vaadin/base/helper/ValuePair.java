a766ad332ee9529aabdc5a7fc902d9225890cb5c
rhirning <rhirning@92c39532-7ad5-11e0-8644-75d2b71920e0>
2016-05-20 06:26:28 +0000

    [maven-release-plugin] copy for tag nicki-parent-6.08

---
+++ nicki/tags/nicki-parent-6.08/nicki-vaadin-base/src/main/java/org/mgnl/nicki/vaadin/base/helper/ValuePair.java
@@ -0,0 +1,20 @@
+package org.mgnl.nicki.vaadin.base.helper;
+
+public class ValuePair {
+	private String name;
+	private String value;
+	
+	
+	public ValuePair(String name, String value) {
+		super();
+		this.name = name;
+		this.value = value;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getValue() {
+		return value;
+	}
+
+}
