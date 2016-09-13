1e39046c624a08ad9f03dfbca103bbf2c260b41d
rhirning <31392706+rhirning@users.noreply.github.com>
2016-09-13 11:13:48 +0000

    Vereinfacht

--- nicki-vaadin-base/src/main/java/org/mgnl/nicki/vaadin/base/helper/DateToStringConverter.java
+++ nicki-vaadin-base/src/main/java/org/mgnl/nicki/vaadin/base/helper/DateToStringConverter.java
@@ -1,53 +1,19 @@
 package org.mgnl.nicki.vaadin.base.helper;
 
-import java.text.ParseException;
-import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Locale;
 
 import org.mgnl.nicki.core.helper.DataHelper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import com.vaadin.data.util.converter.Converter;
+import com.vaadin.data.util.converter.StringToDateConverter;
 
-public class DateToStringConverter implements Converter<String, Date> {
-	private static final long serialVersionUID = 1610290861847281094L;
-	private static final Logger LOG = LoggerFactory.getLogger(DateToStringConverter.class);
+@SuppressWarnings("serial")
+public class DateToStringConverter extends StringToDateConverter {
 
 	@Override
-	public Date convertToModel(String value,
-			Class<? extends Date> targetType, Locale locale)
-			throws Converter.ConversionException {
-		try {
-			return DataHelper.dateFromDisplayDay(value);
-		} catch (ParseException e) {
-			LOG.debug("Error parsing string to date", e);
-		}
-		return null;
+	public DateFormat getFormat(Locale locale){
+		return new SimpleDateFormat(DataHelper.FORMAT_DISPLAY_DAY);
 	}
-
-	@Override
-	public String convertToPresentation(Date value,
-			Class<? extends String> targetType, Locale locale)
-			throws Converter.ConversionException {
-		if (value != null) {
-			return DataHelper.getDisplayDay(value);
-		} else {
-			return "";
-		}
-	}
-
-	@Override
-	public Class<Date> getModelType() {
-		return Date.class;
-	}
-
-	@Override
-	public Class<String> getPresentationType() {
-		return String.class;
-	}
-
-
-
 
 }
