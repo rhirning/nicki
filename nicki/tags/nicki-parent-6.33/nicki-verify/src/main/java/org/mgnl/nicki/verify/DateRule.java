c91ad0ee7d79e82d9ddc4784fd26f640a8779c47
rhirning <rhirning@92c39532-7ad5-11e0-8644-75d2b71920e0>
2016-10-04 09:01:36 +0000

    [maven-release-plugin] copy for tag nicki-parent-6.33

---
+++ nicki/tags/nicki-parent-6.33/nicki-verify/src/main/java/org/mgnl/nicki/verify/DateRule.java
@@ -0,0 +1,49 @@
+package org.mgnl.nicki.verify;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.mgnl.nicki.core.i18n.I18n;
+
+public class DateRule extends Rule {
+	private static final long serialVersionUID = 1L;
+	private static final String DATE_FORMAT = "dd.MM.yyyy";
+	private static final SimpleDateFormat FORMATTER = new SimpleDateFormat(DATE_FORMAT);
+
+	@Override
+	public boolean evaluate(String value, Map<String, String> values) {
+		try {
+			synchronized (FORMATTER) {
+				FORMATTER.parse(value);
+			}
+			String[] parts = StringUtils.split(value, ".");
+			Integer day = Integer.parseInt(parts[0]);
+			if (day < 1 || day > 31) {
+				return false;
+			}
+			Integer month = Integer.parseInt(parts[1]);
+			if (month < 1 || month > 12) {
+				return false;
+			}
+			Integer year = Integer.parseInt(parts[2]);
+			int now = Calendar.getInstance().get(Calendar.YEAR);
+			if (year < now - 200 || year > now + 200) {
+				return false;
+			}
+		} catch (Exception e) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String getMessage() {
+		return I18n.getText(getI18nBase() + ".date");
+	}
+
+	public String toString() {
+		return "date";
+	}
+}
