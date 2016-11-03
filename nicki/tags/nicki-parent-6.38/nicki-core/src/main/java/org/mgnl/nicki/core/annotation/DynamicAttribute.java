1fde26dc24a0ee53603de25cec676ead65ffb112
rhirning <rhirning@92c39532-7ad5-11e0-8644-75d2b71920e0>
2016-11-03 15:32:00 +0000

    [maven-release-plugin] copy for tag nicki-parent-6.38

---
+++ nicki/tags/nicki-parent-6.38/nicki-core/src/main/java/org/mgnl/nicki/core/annotation/DynamicAttribute.java
@@ -0,0 +1,27 @@
+package org.mgnl.nicki.core.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.mgnl.nicki.core.objects.DynamicObject;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD})
+public @interface DynamicAttribute {
+	boolean naming() default false;
+	boolean mandatory() default false;
+	boolean virtual() default false;
+	boolean readonly() default false;
+	String externalName();
+	Class<? extends DynamicObject>[] foreignKey() default {};
+	String editorClass() default "";
+	String searchFieldClass() default "";
+	boolean search() default false;
+	String caption() default "";
+	String format() default "";
+	Class<?> type() default String.class;
+	org.mgnl.nicki.core.objects.DynamicAttribute.CREATEONLY createOnly()
+		default org.mgnl.nicki.core.objects.DynamicAttribute.CREATEONLY.FALSE;
+}
