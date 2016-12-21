2b729efdd06451c29f8b9e5392d7fd1f87c70c71
rhirning <rhirning@92c39532-7ad5-11e0-8644-75d2b71920e0>
2016-12-21 10:43:16 +0000

    [maven-release-plugin] copy for tag nicki-parent-6.49

---
+++ nicki/tags/nicki-parent-6.49/nicki-idm-novell-ws/src/test/java/org/mgnl/nicki/idm/novell/ws/TestWS.java
@@ -0,0 +1,56 @@
+package org.mgnl.nicki.idm.novell.ws;
+
+import java.rmi.RemoteException;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.xml.rpc.ServiceException;
+
+import com.novell.soa.af.impl.soap.AdminException;
+import com.novell.soa.af.impl.soap.ProcessArray;
+import com.novell.soa.af.impl.soap.Provisioning;
+import com.novell.soa.af.impl.soap.ProvisioningService;
+import com.novell.soa.ws.portable.Stub;
+
+public class TestWS {
+	private static String USERNAME = "cn=admin,ou=sa,o=system";
+	private static String PASSWORD = "netiq000";
+	private static String URL = "http://172.17.2.91:8180/IDMProv/provisioning/service?wsdl";
+
+	public static void main(String[] args) throws NamingException, ServiceException, RemoteException, AdminException {
+		InitialContext ctx = new InitialContext();
+		ProvisioningService service = (ProvisioningService) ctx
+				.lookup("xmlrpc:soap:com.novell.soa.af.impl.soap.ProvisioningService");
+		Provisioning prov = service.getProvisioningPort();
+
+		Stub stub = (Stub) prov;
+		// set username and password
+		stub._setProperty(Stub.USERNAME_PROPERTY, USERNAME);
+		stub._setProperty(Stub.PASSWORD_PROPERTY, PASSWORD);
+		// set the endpoint URL
+		stub._setProperty(Stub.ENDPOINT_ADDRESS_PROPERTY, URL);
+
+		// invoke the getAllProcesses method
+		ProcessArray array = prov.getAllProcesses();
+		com.novell.soa.af.impl.soap.Process[] procs = array.getProcess();
+		// print process array
+		System.out.println("list of all processes:");
+		if (procs != null) {
+			for (int i = 0; i < procs.length; i++) {
+				System.out.println(" process with request identifier " + procs[i].getRequestId());
+				System.out.println(" initiator = " + procs[i].getInitiator());
+				System.out.println(" recipient = " + procs[i].getRecipient());
+				System.out.println(" processId = " + procs[i].getProcessId());
+				// procs[i].getCreationTime().getTime());
+				if (null != procs[i].getCompletionTime()) {
+					System.out.println(" completed = " + procs[i].getCompletionTime().getTime());
+				}
+				System.out.println(" approval status = " + procs[i].getApprovalStatus());
+				System.out.println(" process status = " + procs[i].getProcessStatus());
+				if (i != procs.length - 1)
+					System.out.println();
+			}
+		}
+	}
+
+}
