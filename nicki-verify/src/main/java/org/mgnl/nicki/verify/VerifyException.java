d5d6078aa27d2b77b2c259416885c6992355bdce
rhirning <31392706+rhirning@users.noreply.github.com>
2017-03-03 14:08:00 +0000

    ReferencedError

--- nicki-verify/src/main/java/org/mgnl/nicki/verify/VerifyException.java
+++ nicki-verify/src/main/java/org/mgnl/nicki/verify/VerifyException.java
@@ -1,27 +1,9 @@
 package org.mgnl.nicki.verify;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @SuppressWarnings("serial")
 public class VerifyException extends Exception {
-	private List<String> errors;
-
-	public VerifyException(List<String> errors) {
-		this.errors = errors;
-	}
 
 	public VerifyException(String message) {
-		errors = new ArrayList<>();
-		errors.add(message);
-	}
-
-	public List<String> getErrors() {
-		return errors;
-	}
-
-	@Override
-	public String getMessage() {
-		return errors == null ? null : errors.toString();
+		super(message);
 	}
 }
