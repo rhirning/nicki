62443dfb6bea56e6c398b64881310355258363ac
rhirning <rhirning@92c39532-7ad5-11e0-8644-75d2b71920e0>
2017-03-13 08:21:30 +0000

    [maven-release-plugin] copy for tag nicki-parent-7.01

---
+++ nicki/tags/nicki-parent-7.01/nicki-core/src/main/java/org/mgnl/nicki/core/data/InvalidActionException.java
@@ -0,0 +1,10 @@
+package org.mgnl.nicki.core.data;
+
+@SuppressWarnings("serial")
+public class InvalidActionException extends Exception {
+
+	public InvalidActionException(String message) {
+		super(message);
+	}
+
+}
