fa0c7043e7f6c66651c8ea16d19afccfd920ff23
rhirning <rhirning@92c39532-7ad5-11e0-8644-75d2b71920e0>
2017-05-31 13:21:45 +0000

    packages

---
+++ nicki/trunk/nicki-scheduler/src/main/java/org/mgnl/nicki/scheduler/JobConfigurations.java
@@ -0,0 +1,17 @@
+package org.mgnl.nicki.scheduler;
+
+import java.util.List;
+
+public class JobConfigurations {
+	private List<JobConfig> jobConfig;
+
+	public List<JobConfig> getJobConfig() {
+		return jobConfig;
+	}
+
+	public void setJobConfig(List<JobConfig> jobConfig) {
+		this.jobConfig = jobConfig;
+	}
+
+
+}
