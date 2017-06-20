c2af3a164ea7b60b8614e333e5f11a8b8b78a5f6
rhirning <rhirning@92c39532-7ad5-11e0-8644-75d2b71920e0>
2017-06-20 10:21:39 +0000

    [maven-release-plugin] copy for tag nicki-parent-7.27

---
+++ nicki/tags/nicki-parent-7.27/nicki-db/src/main/java/org/mgnl/nicki/db/statistics/StatisticsManager.java
@@ -0,0 +1,64 @@
+package org.mgnl.nicki.db.statistics;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.mgnl.nicki.core.config.Config;
+import org.mgnl.nicki.core.util.Classes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StatisticsManager {
+	private static final Logger LOG = LoggerFactory.getLogger(StatisticsManager.class);
+	private static StatisticsManager instance;
+	private Map<String, Map<String, Statistics>> statisticsMap;
+
+	public static boolean isAvailable(String businessCategory, String statisticName) {
+		return getInstance().isStatisticsAvailable(businessCategory, statisticName);
+	}
+
+	public static Map<String, String> getStatistics(String businessCategory, String statisticName, Map<String, String> input)
+			throws InvalidStatisticsException, MissingDataException, StatisticsException {
+		return getInstance().executeStatistics(businessCategory, statisticName, input);
+	}
+	
+	private Map<String, String> executeStatistics(String businessCategory, String statisticName, Map<String, String> input) throws InvalidStatisticsException, StatisticsException, MissingDataException {
+		if (!isAvailable(businessCategory, statisticName) ) {
+			throw new InvalidStatisticsException(businessCategory + "/" + statisticName);
+		}
+		Statistics statistics = statisticsMap.get(businessCategory).get(statisticName);
+		return statistics.execute(input);
+	}
+
+	private static synchronized StatisticsManager getInstance() {
+		if (instance == null) {
+			instance = new StatisticsManager();
+			instance.init();
+		}
+		return instance;
+	}
+
+	private void init() {
+		statisticsMap = new HashMap<>();
+		String factoryClass = Config.getProperty("nicki.db.statistics.factory", "org.mgnl.nicki.db.statistics.ClasspathStatisticsFactory");
+		try {
+			StatisticsFactory factory = Classes.newInstance(factoryClass);
+			for (Statistics statistics : factory.load()) {
+				String category = statistics.getCategory();
+				String name = statistics.getName();
+				if (!statisticsMap.containsKey(category)) {
+					statisticsMap.put(category, new HashMap<String, Statistics>());
+				}
+				Map<String, Statistics> bcMap = statisticsMap.get(category);
+				bcMap.put(name, statistics);
+			}
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+			LOG.error("Error loading StatisticsFactory", e);
+		}
+	}
+
+	private boolean isStatisticsAvailable(String businessCategory, String statisticName) {
+		return statisticsMap.containsKey(businessCategory) && statisticsMap.get(businessCategory).containsKey(statisticName);
+	}
+
+}
