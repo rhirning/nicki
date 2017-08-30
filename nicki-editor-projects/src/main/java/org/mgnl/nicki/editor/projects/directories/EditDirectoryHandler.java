d539036fe25b7bbf17c1c1d794f4497d36621108
rhirning <31392706+rhirning@users.noreply.github.com>
2017-08-30 12:16:58 +0000

    - Apache 2.0 License header - JAAS config

--- nicki-editor-projects/src/main/java/org/mgnl/nicki/editor/projects/directories/EditDirectoryHandler.java
+++ nicki-editor-projects/src/main/java/org/mgnl/nicki/editor/projects/directories/EditDirectoryHandler.java
@@ -1,22 +1,18 @@
 /**
- * Copyright (c) 2003-2015 Dr. Ralf Hirning
- * All rights reserved.
- *  
- * This program is dual-licensed under both the GNU General
- * Public License and an individual license with Dr. Ralf
- * Hirning.
- * 
- * 1. For the GPL license (GPL), you can redistribute and/or
- * modify this file under the terms of the GNU Public License v3.0
- * which is available at
- * http://www.gnu.org/licenses/gpl.html
- * 2. For the individual license, this file and the accompanying
- * materials are made available under the terms of the
- * individual license.
- * 
- * Any modifications to this file must keep this entire header
- * intact.
-*/
+ * Copyright © 2017 Ralf Hirning (ralf@hirning.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.mgnl.nicki.editor.projects.directories;
 
 public interface EditDirectoryHandler {
