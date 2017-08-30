e8a938a981ea993b411a0a9cebd7f70a522ddd43
rhirning <rhirning@92c39532-7ad5-11e0-8644-75d2b71920e0>
2017-08-30 12:16:58 +0000

    - Apache 2.0 License header - JAAS config

--- nicki/trunk/nicki-core/src/test/java/HelloKDC.java
+++ nicki/trunk/nicki-core/src/test/java/HelloKDC.java
@@ -1,21 +1,18 @@
-/* 
- * Copyright (C) 2009  "Darwin V. Felix" <dfelix@users.sourceforge.net>
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+/**
+ * Copyright © 2017 Ralf Hirning (ralf@hirning.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
- 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.security.NoSuchAlgorithmException;
@@ -39,7 +36,7 @@ public final class HelloKDC {
         final String username = "rhirning";
         
         // Password for the pre-auth acct.
-        final String password = "M34mandelboot";
+        final String password = "XXX";
         
         // Name of our krb5 config file
         final String krbfile = "D:/work/svn/nicki/nicki-core/src/test/resources/krb5.conf";
