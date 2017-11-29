ef38bca3ef5af508c7c3a898007238a1ddc2c18f
Ralf Hirning <ralf@hirning.de>
2017-11-29 09:36:04 +0100

    LoginContext with two NickiContext (login, backend)

---
+++ nicki-core/src/main/java/org/mgnl/nicki/core/context/LoginContext.java
@@ -0,0 +1,43 @@
+package org.mgnl.nicki.core.context;
+
+/*-
+ * #%L
+ * nicki-core
+ * %%
+ * Copyright (C) 2017 Ralf Hirning
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+class LoginContext {
+	private NickiContext context;
+	private NickiContext loginContext;
+
+	public NickiContext getContext() {
+		return this.context;
+	}
+
+	public void setContext(NickiContext context) {
+		this.context = context;
+	}
+
+	public NickiContext getLoginContext() {
+		return this.loginContext;
+	}
+
+	public void setLoginContext(NickiContext loginContext) {
+		this.loginContext = loginContext;
+	}
+
+}
