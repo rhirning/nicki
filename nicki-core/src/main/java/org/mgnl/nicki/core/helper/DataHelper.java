5b81c6bad4cbf01a21de690a585e6d2926a50572
Ralf Hirning <ralf@hirning.de>
2021-03-02 09:52:59 +0100

    compare Date

--- nicki-core/src/main/java/org/mgnl/nicki/core/helper/DataHelper.java
+++ nicki-core/src/main/java/org/mgnl/nicki/core/helper/DataHelper.java
@@ -89,6 +89,18 @@ public class DataHelper {
 		}
 		return list;
 	}
+	
+	public static int compareTo(Date date1, Date date2) {
+		if (date1 == null && date2 == null) {
+			return 0;
+		} else if (date1 == null) {
+			return -1;
+		} else if (date2 == null) {
+			return 1;
+		} else {
+			return date1.compareTo(date2);
+		}
+	}
         
 
 	/**
