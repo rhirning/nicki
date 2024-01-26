8f412cd610addd2ddc01bca401d2842c5998644e
Ralf Hirning <ralf@hirning.de>
2024-01-26 14:02:28 +0100

    nicki-context-db entfernt, lib-Versionen aktualisiert

--- nicki-mq/nicki-mq-broker/src/main/java/org/mgnl/nicki/mq/broker/ActiveMQContextListener.java
+++ nicki-mq/nicki-mq-broker/src/main/java/org/mgnl/nicki/mq/broker/ActiveMQContextListener.java
@@ -4,7 +4,7 @@ package org.mgnl.nicki.mq.broker;
  * #%L
  * nicki-mq-broker
  * %%
- * Copyright (C) 2017 Ralf Hirning
+ * Copyright (C) 2017 - 2024 Ralf Hirning
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,29 +19,9 @@ package org.mgnl.nicki.mq.broker;
  * limitations under the License.
  * #L%
  */
+
+
 import java.util.HashMap;
-
-/*-
- * #%L
- * nicki-mq
- * %%
- * Copyright (C) 2017 Ralf Hirning
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-
-
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
